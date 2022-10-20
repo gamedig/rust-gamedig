13c296b5a952b115de61553cb0116b069a8767a6
CosminPerRam <cosmin.p@live.com>
2022-10-20 22:26:05 +0300

    tf2 skeleton

--- src/games/tf2.rs
+++ src/games/tf2.rs
@@ -1,12 +1,26 @@
 use crate::errors::GDError;
-use crate::valve::{ValveProtocol, App, GatheringSettings, Response};
+use crate::valve;
+use crate::valve::{ValveProtocol, App, GatheringSettings};
+
+#[derive(Debug)]
+pub struct Response {
+
+}
+
+impl Response {
+    pub fn new_from_valve_response(response: valve::Response) -> Self {
+
+    }
+}
 
 pub fn query(address: &str, port: Option<u16>) -> Result<Response, GDError> {
-    ValveProtocol::query(App::TF2, address, match port {
+    let valve_response = ValveProtocol::query(App::TF2, address, match port {
         None => 27015,
         Some(port) => port
     }, GatheringSettings {
         players: true,
         rules: true
-    })
+    })?;
+
+    Ok(Response::new_from_valve_response(valve_response))
 }
