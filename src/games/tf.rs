8c5ac24468ede985c353d0cdb6396e8f02f6a46a
CosminPerRam <cosmin.p@live.com>
2022-11-26 16:15:26 +0200

    Changed The Forest default port to 27016.

--- src/games/tf.rs
+++ src/games/tf.rs
@@ -4,7 +4,7 @@ use crate::protocols::valve::{game, SteamID};
 
 pub fn query(address: &str, port: Option<u16>) -> GDResult<game::Response> {
     let valve_response = valve::query(address, match port {
-        None => 27015,
+        None => 27016,
         Some(port) => port
     }, SteamID::TF.as_app(), None, None)?;
 
