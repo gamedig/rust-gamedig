5d33087c17eaf3168c0702de08238635a76a8b73
CosminPerRam <cosmin.p@live.com>
2023-02-24 19:59:05 +0200

    Furter port to accept more packets

--- src/protocols/gamespy/protocol/one.rs
+++ src/protocols/gamespy/protocol/one.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use crate::bufferer::{Bufferer, Endianess};
-use crate::GDResult;
+use crate::{GDError, GDResult};
 use crate::protocols::gamespy::Response;
 use crate::protocols::types::TimeoutSettings;
 use crate::socket::{Socket, UdpSocket};
@@ -11,26 +11,78 @@ pub fn query(address: &str, port: u16, timeout_settings: Option<TimeoutSettings>
 
     socket.send("\\status\\xserverquery".as_bytes())?;
 
-    let data = socket.receive(None)?;
-    let mut bufferer = Bufferer::new_with_data(Endianess::Little, &data);
+    let mut receivedQueryId: Option<usize> = None;
+    let mut parts: Vec<usize> = Vec::new();
+    let mut maxPartNum: Option<usize> = None;
 
-    let mut as_string = bufferer.get_string_utf8_unended()?;
-    as_string.remove(0);
+    let mut serverValues = HashMap::new();
 
-    let splited: Vec<&str> = as_string.split('\\').collect();
-    let mut data = HashMap::new();
+    while maxPartNum.is_none() {
+        let data = socket.receive(None)?;
+        let mut bufferer = Bufferer::new_with_data(Endianess::Little, &data);
 
-    for i in 0..splited.len() / 2 {
-        let position = i * 2;
-        let key = splited[position];
-        let value = splited.get(position + 1).unwrap_or(&"");
+        let mut as_string = bufferer.get_string_utf8_unended()?;
+        as_string.remove(0);
 
-        data.insert(key, value);
+        let splited: Vec<String> = as_string.split('\\').map(str::to_string).collect();
+
+        for i in 0..splited.len() / 2 {
+            let position = i * 2;
+            let key = splited[position].clone();
+            let value = match splited.get(position + 1) {
+                None => "".to_string(),
+                Some(v) => v.clone()
+            };
+
+            serverValues.insert(key, value);
+        }
+
+        let isFinal = serverValues.contains_key("final");
+        let queryData = serverValues.get("queryid");
+        let mut part = None;
+        let mut queryId = None;
+        if let Some(qid) = queryData {
+            let split: Vec<&str> = qid.split('.').collect();
+            if split.len() > 1 {
+                part = Some(split[1].parse::<usize>().unwrap());
+            }
+            queryId = Some(split[0].parse::<usize>().unwrap());
+        }
+
+        serverValues.remove("final");
+        serverValues.remove("queryid");
+
+        println!("{:?} {:?} {:?}", part, queryId, isFinal);
+
+        if receivedQueryId.is_some() && receivedQueryId != queryId {
+            println!("Rejected packet, wrong query ID");
+            return Err(GDError::PacketBad);
+        }
+        else {
+            receivedQueryId = queryId;
+        }
+
+        if part.is_none() {
+            part = Some(parts.len());
+            println!("No part number received, assigned: {:?}", part.unwrap());
+        }
+        else {
+            let part_n = part.unwrap();
+            if parts.contains(&part_n) {
+                println!("Rejected packet (duplicate)");
+                return Err(GDError::PacketBad);
+            }
+            else {
+                parts.push(part_n);
+            }
+        }
+
+        if isFinal {
+            maxPartNum = part;
+        }
     }
 
-    println!("{:#?}", data);
-    println!("{}", data.contains_key("final"));
-    println!("{}", data["queryid"]);
+    println!("{:#?}", serverValues);
 
     Ok(Response {
 
