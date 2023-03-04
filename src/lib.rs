8992ffe4dfbf9be52e23bad98b78815099fff320
CosminPerRam <cosmin.p@live.com>
2023-03-04 14:04:38 +0200

    [Docs] Match the docs example to the one from the readme

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,15 @@
 //! # An usage example:
 //!
 //! ```no_run
-//! use gamedig::games::tf2;
+//!use gamedig::games::tf2;
 //!
-//! fn main() {
-//!     let response = tf2::query("91.216.250.10", None); //or Some(27015), None is the default protocol port
-//!     match response {
-//!         Err(error) => println!("Couldn't query, error: {}", error),
-//!         Ok(r) => println!("{:?}", r)
-//!     }
-//! }
+//!fn main() {
+//!    let response = tf2::query("127.0.0.1", None); // None is the default port (which is 27015), could also be Some(27015)
+//!    match response { // Result type, must check what it is...
+//!        Err(error) => println!("Couldn't query, error: {}", error),
+//!        Ok(r) => println!("{:#?}", r)
+//!    }
+//!}
 //! ```
 //!
 //! # Crate features:
