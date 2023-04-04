3c157a7f316297f9782b34947bcffcaea46d0b60
Cain <75994858+cainthebest@users.noreply.github.com>
2023-04-04 22:14:16 +0100

    impl: player and team structure

--- src/protocols/gamespy/types.rs
+++ src/protocols/gamespy/types.rs
@@ -43,40 +43,93 @@ pub mod one {
 }
 
 pub mod two {
+    pub(crate) struct RequestPacket {
+        /// The header is a 64-bit signed integer, but we only need the
+        /// first 16 bits as the header should always be
+        /// `0xFEFD`.
+        pub(crate) header: u16,
+        /// The delimiter, which is always `0x00`.
+        pub(crate) delimiter: u8,
+        /// The ping value, This can be anything you want, use it to make
+        /// sure the response is valid.
+        pub(crate) ping_value: [u8; 4],
+        /// Whether to return the server info and rules.
+        /// `0x00` = No
+        /// `0xFF` = Yes
+        pub(crate) server_info_and_rules: u8,
+        /// Whether to return the player info.
+        /// `0x00` = No
+        /// `0xFF` = Yes
+        pub(crate) player_info: u8,
+        /// Whether to return the team info.
+        /// `0x00` = No
+        /// `0xFF` = Yes
+        pub(crate) team_info: u8,
+    }
+
+    impl RequestPacket {
+        /// Converts the request packet to a byte array of 10 bytes.
+        /// This is the format that the server expects.
+        #[allow(dead_code)]
+        const fn to_u8_array(&self) -> [u8; 10] {
+            let header_byte: [u8; 2] = self.header.to_le_bytes();
+
+            [
+                header_byte[0],
+                header_byte[1],
+                self.delimiter,
+                self.ping_value[0],
+                self.ping_value[1],
+                self.ping_value[2],
+                self.ping_value[3],
+                self.server_info_and_rules,
+                self.player_info,
+                self.team_info,
+            ]
+        }
+
+        pub(crate) const REQUEST_PACKET_BYTES: [u8; 10] = RequestPacket {
+            header: 0xFEFD,
+            delimiter: 0x00,
+            // The ping value is referred to as "CORY" because it was originally documented by Cory,
+            // who created a C library a long time ago. His documentation has since been adopted by many
+            // other libraries and is still in use today, as it provides valuable information.
+            ping_value: [b'C', b'O', b'R', b'Y'],
+            server_info_and_rules: 0xFF,
+            player_info: 0xFF,
+            team_info: 0xFF,
+        }
+        .to_u8_array();
+    }
+
+    /// A Flag is a representation of a boolean value from a string.
+    #[derive(Debug, PartialEq)]
+    pub enum Flag {
+        Enabled,
+        Disabled,
+        Unknown,
+    }
+
+    impl Flag {
+        fn from_str(s: &str) -> Self {
+            match s.trim().to_lowercase().as_str() {
+                "yes" | "on" | "1" => Flag::Enabled,
+                "no" | "off" | "0" => Flag::Disabled,
+                _ => Flag::Unknown,
+            }
+        }
+
+        pub fn to_bool(&self) -> bool {
+            match self {
+                Flag::Enabled => true,
+                Flag::Disabled => false,
+                Flag::Unknown => false,
+            }
+        }
+    }
 
     pub mod server_info {
-
-        /// A Flag is a representation of a boolean value from a string.
-        #[derive(Debug, PartialEq)]
-        pub enum Flag {
-            Enabled,
-            Disabled,
-            Unknown,
-        }
-
-        pub(crate) struct RequestPacket {
-            /// The header is a 64-bit signed integer, but we only need the
-            /// first 16 bits as the header should always be
-            /// `0xFEFD`.
-            pub(crate) header: u16,
-            /// The delimiter, which is always `0x00`.
-            pub(crate) delimiter: u8,
-            /// The ping value, This can be anything you want, use it to make
-            /// sure the response is valid.
-            pub(crate) ping_value: [u8; 4],
-            /// Whether to return the server info and rules.
-            /// `0x00` = No
-            /// `0xFF` = Yes
-            pub(crate) server_info_and_rules: u8,
-            /// Whether to return the player info.
-            /// `0x00` = No
-            /// `0xFF` = Yes
-            pub(crate) player_info: u8,
-            /// Whether to return the team info.
-            /// `0x00` = No
-            /// `0xFF` = Yes
-            pub(crate) team_info: u8,
-        }
+        use super::Flag;
 
         #[derive(Debug, PartialEq)]
         pub struct ServerConnection {
@@ -89,11 +142,11 @@ pub mod two {
         pub struct GameInfo {
             pub map_name: String,
             pub game_type: String,
-            pub num_players: usize,
-            pub max_players: usize,
+            pub num_players: u8,
+            pub max_players: u8,
             pub game_mode: String,
             pub game_version: String,
-            pub status: usize,
+            pub status: u8,
             pub game_id: String,
             pub map_id: String,
         }
@@ -101,10 +154,10 @@ pub mod two {
         #[derive(Debug, PartialEq, Default)]
         pub struct GameplaySettings {
             pub sv_punkbuster: Option<Flag>,
-            pub time_limit: Option<usize>,
-            pub num_rounds: Option<usize>,
-            pub spawn_wave_time: Option<usize>,
-            pub spawn_delay: Option<usize>,
+            pub time_limit: Option<u8>,
+            pub num_rounds: Option<u8>,
+            pub spawn_wave_time: Option<u8>,
+            pub spawn_delay: Option<u8>,
         }
 
         #[derive(Debug, PartialEq, Default)]
@@ -117,14 +170,14 @@ pub mod two {
 
         #[derive(Debug, PartialEq, Default)]
         pub struct TeamSettings {
-            pub us_team_ratio: Option<usize>,
-            pub nva_team_ratio: Option<usize>,
+            pub us_team_ratio: Option<u8>,
+            pub nva_team_ratio: Option<u8>,
         }
 
         #[derive(Debug, PartialEq, Default)]
         pub struct CameraSettings {
-            pub name_tag_distance: Option<usize>,
-            pub name_tag_distance_scope: Option<usize>,
+            pub name_tag_distance: Option<u16>,
+            pub name_tag_distance_scope: Option<u16>,
             pub allow_nose_cam: Option<String>,
             pub external_view: Option<String>,
             pub free_camera: Option<Flag>,
@@ -132,7 +185,7 @@ pub mod two {
 
         #[derive(Debug, PartialEq, Default)]
         pub struct GameStartSettings {
-            pub game_start_delay: Option<usize>,
+            pub game_start_delay: Option<u8>,
             pub ticket_ratio: Option<String>,
             pub kickback: Option<String>,
             pub kickback_on_splash: Option<String>,
@@ -145,7 +198,7 @@ pub mod two {
             pub cpu: Option<usize>,
             // ! Type is unknown, set as string as its unknown if it's a number or a percentage
             pub bot_skill: Option<String>,
-            pub reserved_slots: Option<usize>,
+            pub reserved_slots: Option<u16>,
             // ! Type is unknown (probably a key-value pair)
             pub active_mods: Option<String>,
         }
@@ -161,218 +214,33 @@ pub mod two {
             pub game_start_settings: GameStartSettings,
             pub config: ServerConfig,
         }
+    }
 
-        impl RequestPacket {
-            /// Converts the request packet to a byte array of 10 bytes.
-            /// This is the format that the server expects.
-            #[allow(dead_code)]
-            const fn to_u8_array(&self) -> [u8; 10] {
-                let header_byte: [u8; 2] = self.header.to_le_bytes();
-
-                [
-                    header_byte[0],
-                    header_byte[1],
-                    self.delimiter,
-                    self.ping_value[0],
-                    self.ping_value[1],
-                    self.ping_value[2],
-                    self.ping_value[3],
-                    self.server_info_and_rules,
-                    self.player_info,
-                    self.team_info,
-                ]
-            }
-
-            pub(crate) const REQUEST_PACKET_BYTES: [u8; 10] = RequestPacket {
-                header: 0xFEFD,
-                delimiter: 0x00,
-                // The ping value is referred to as "CORY" because it was originally documented by Cory,
-                // who created a C library a long time ago. His documentation has since been adopted by many
-                // other libraries and is still in use today, as it provides valuable information.
-                ping_value: [b'C', b'O', b'R', b'Y'],
-                server_info_and_rules: 0xFF,
-                player_info: 0xFF,
-                team_info: 0xFF,
-            }
-            .to_u8_array();
+    pub mod player_info {
+        pub struct Player {
+            pub name: String,
+            pub score: Option<u16>,
+            pub deaths: Option<u16>,
+            pub ping: Option<u32>,
+            pub team: Option<u8>,
+            pub kills: Option<u16>,
         }
 
-        /// The bytes of the request packet to be sent to the server
-        #[allow(dead_code)]
-
-        impl Flag {
-            fn from_str(s: &str) -> Self {
-                match s.trim().to_lowercase().as_str() {
-                    "yes" | "on" | "1" => Flag::Enabled,
-                    "no" | "off" | "0" => Flag::Disabled,
-                    _ => Flag::Unknown,
-                }
-            }
-
-            pub fn to_bool(&self) -> bool {
-                match self {
-                    Flag::Enabled => true,
-                    Flag::Disabled => false,
-                    Flag::Unknown => false,
-                }
-            }
+        pub struct PlayerInfo {
+            pub players: Vec<Player>,
         }
+    }
 
-        impl ServerConnection {
-            pub fn new(hostname: String, hostport: u16, password: Flag) -> Self {
-                ServerConnection {
-                    hostname,
-                    hostport,
-                    password,
-                }
-            }
+    pub mod team_info {
+        pub struct TeamInfo {
+            // ! Team info structure is not not known yet
+            pub data: String,
         }
+    }
 
-        impl GameInfo {
-            pub fn new(
-                map_name: String,
-                game_type: String,
-                num_players: usize,
-                max_players: usize,
-                game_mode: String,
-                game_version: String,
-                status: usize,
-                game_id: String,
-                map_id: String,
-            ) -> Self {
-                GameInfo {
-                    map_name,
-                    game_type,
-                    num_players,
-                    max_players,
-                    game_mode,
-                    game_version,
-                    status,
-                    game_id,
-                    map_id,
-                }
-            }
-        }
-
-        impl GameplaySettings {
-            pub fn new(
-                sv_punkbuster: Option<Flag>,
-                time_limit: Option<usize>,
-                num_rounds: Option<usize>,
-                spawn_wave_time: Option<usize>,
-                spawn_delay: Option<usize>,
-            ) -> Self {
-                GameplaySettings {
-                    sv_punkbuster,
-                    time_limit,
-                    num_rounds,
-                    spawn_wave_time,
-                    spawn_delay,
-                }
-            }
-        }
-
-        impl FriendlyFireSettings {
-            pub fn new(
-                soldier_friendly_fire: Option<Flag>,
-                vehicle_friendly_fire: Option<Flag>,
-                soldier_friendly_fire_on_splash: Option<String>,
-                vehicle_friendly_fire_on_splash: Option<String>,
-            ) -> Self {
-                FriendlyFireSettings {
-                    soldier_friendly_fire,
-                    vehicle_friendly_fire,
-                    soldier_friendly_fire_on_splash,
-                    vehicle_friendly_fire_on_splash,
-                }
-            }
-        }
-
-        impl TeamSettings {
-            pub fn new(us_team_ratio: Option<usize>, nva_team_ratio: Option<usize>) -> Self {
-                TeamSettings {
-                    us_team_ratio,
-                    nva_team_ratio,
-                }
-            }
-        }
-
-        impl CameraSettings {
-            pub fn new(
-                name_tag_distance: Option<usize>,
-                name_tag_distance_scope: Option<usize>,
-                allow_nose_cam: Option<String>,
-                external_view: Option<String>,
-                free_camera: Option<Flag>,
-            ) -> Self {
-                CameraSettings {
-                    name_tag_distance,
-                    name_tag_distance_scope,
-                    allow_nose_cam,
-                    external_view,
-                    free_camera,
-                }
-            }
-        }
-
-        impl GameStartSettings {
-            pub fn new(
-                game_start_delay: Option<usize>,
-                ticket_ratio: Option<String>,
-                kickback: Option<String>,
-                kickback_on_splash: Option<String>,
-                auto_balance: Option<Flag>,
-            ) -> Self {
-                GameStartSettings {
-                    game_start_delay,
-                    ticket_ratio,
-                    kickback,
-                    kickback_on_splash,
-                    auto_balance,
-                }
-            }
-        }
-
-        impl ServerConfig {
-            pub fn new(
-                dedicated: Option<Flag>,
-                cpu: Option<usize>,
-                bot_skill: Option<String>,
-                reserved_slots: Option<usize>,
-                active_mods: Option<String>,
-            ) -> Self {
-                ServerConfig {
-                    dedicated,
-                    cpu,
-                    bot_skill,
-                    reserved_slots,
-                    active_mods,
-                }
-            }
-        }
-
-        impl ServerInfo {
-            pub fn new(
-                connection: ServerConnection,
-                game_info: GameInfo,
-                gameplay_settings: GameplaySettings,
-                friendly_fire_settings: FriendlyFireSettings,
-                team_settings: TeamSettings,
-                camera_settings: CameraSettings,
-                game_start_settings: GameStartSettings,
-                config: ServerConfig,
-            ) -> Self {
-                ServerInfo {
-                    connection,
-                    game_info,
-                    gameplay_settings,
-                    friendly_fire_settings,
-                    team_settings,
-                    camera_settings,
-                    game_start_settings,
-                    config,
-                }
-            }
-        }
+    pub struct Response {
+        pub server_info: server_info::ServerInfo,
+        pub player_info: player_info::PlayerInfo,
+        pub team_info: team_info::TeamInfo,
     }
 }
