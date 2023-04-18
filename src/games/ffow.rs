b4b6c9b034380435c2722e264cbb5c1837d16eaa
CosminPerRam <cosmin.p@live.com>
2023-04-19 01:22:41 +0300

    [Game] Add query_with_timeout

--- src/games/ffow.rs
+++ src/games/ffow.rs
@@ -1,3 +1,4 @@
+use crate::protocols::types::TimeoutSettings;
 use crate::protocols::valve::{Engine, Environment, Server, ValveProtocol};
 use crate::GDResult;
 #[cfg(feature = "serde")]
@@ -37,7 +38,11 @@ pub struct Response {
 }
 
 pub fn query(address: &str, port: Option<u16>) -> GDResult<Response> {
-    let mut client = ValveProtocol::new(address, port.unwrap_or(5478), None)?;
+    query_with_timeout(address, port, TimeoutSettings::default())
+}
+
+pub fn query_with_timeout(address: &str, port: Option<u16>, timeout_settings: TimeoutSettings) -> GDResult<Response> {
+    let mut client = ValveProtocol::new(address, port.unwrap_or(5478), Some(timeout_settings))?;
     let mut buffer = client.get_request_data(
         &Engine::GoldSrc(true),
         0,
