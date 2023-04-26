565d1c2819e7e0b8814e69dfbc7dfbaad7af3b7c
CosminPerRam <cosmin.p@live.com>
2023-04-27 00:25:54 +0300

    [Service] Add filters

--- src/services/valve_master_server/service.rs
+++ src/services/valve_master_server/service.rs
@@ -1,10 +1,11 @@
+use crate::bufferer::{Bufferer, Endianess};
 use crate::socket::{Socket, UdpSocket};
-use crate::valve_master_server::{Filters, Region};
-use crate::GDResult;
+use crate::valve_master_server::{Region, SearchFilters};
+use crate::{GDError, GDResult};
 
-fn construct_payload(region: Region, filters: Option<Filters>, last_ip: String, last_port: u16) -> Vec<u8> {
-    let filters_bytes: &[u8] = match filters {
-        None => &[0x00],
+fn construct_payload(region: Region, filters: Option<SearchFilters>, last_ip: String, last_port: u16) -> Vec<u8> {
+    let filters_bytes: Vec<u8> = match filters {
+        None => vec![0x00],
         Some(f) => f.to_bytes(),
     };
 
@@ -24,29 +25,52 @@ fn construct_payload(region: Region, filters: Option<Filters>, last_ip: String, 
         // Which needs to end with a NULL byte
         &[0x00],
         // Then the filters
-        filters_bytes,
+        &filters_bytes,
     ]
     .concat()
 }
 
-pub fn query(region: Region, filters: Option<Filters>) -> GDResult<Vec<String>> {
+pub fn query(region: Region, search_filters: Option<SearchFilters>) -> GDResult<Vec<String>> {
     let mut socket = UdpSocket::new("hl2master.steampowered.com", 27011)?;
     socket.apply_timeout(None)?;
 
-    let initial_payload = construct_payload(region, filters, "0.0.0.0".to_string(), 0);
+    let initial_payload = construct_payload(region, search_filters, "0.0.0.0".to_string(), 0);
     println!("{:02X?}", initial_payload);
     socket.send(&initial_payload)?;
 
     let received_data = socket.receive(Some(1400))?;
-    println!("{:02X?}", received_data);
+    let mut buf = Bufferer::new_with_data(Endianess::Big, &received_data);
+
+    if buf.get_u32()? != 4294967295 || buf.get_u16()? != 26122 {
+        return Err(GDError::PacketBad);
+    }
+
+    while buf.remaining_length() > 0 {
+        let first_octet = buf.get_u8()?;
+        let second_octet = buf.get_u8()?;
+        let third_octet = buf.get_u8()?;
+        let fourth_octet = buf.get_u8()?;
+
+        let port = buf.get_u16()?;
+
+        println!("{first_octet}.{second_octet}.{third_octet}.{fourth_octet}:{port}");
+    }
+
+    println!("{:02X?}", buf.remaining_data());
 
     Ok(Vec::new())
 }
 
 #[cfg(test)]
 mod master_query {
-    use crate::valve_master_server::{query, Region};
+    use crate::valve_master_server::{query, Filter, Region, SearchFilters};
 
     #[test]
-    fn test_stuff() { query(Region::Europe, None).unwrap(); }
+    fn test_stuff() {
+        let search_filters = SearchFilters::new()
+            .add(Filter::AppId(440))
+            .add(Filter::CanHavePassword(false));
+
+        query(Region::Europe, Some(search_filters)).unwrap();
+    }
 }
--- src/services/valve_master_server/types.rs
+++ src/services/valve_master_server/types.rs
@@ -1,15 +1,99 @@
-#[derive(Eq, PartialEq, Clone)]
-pub struct Filters {
-    pub secure: bool,
-    pub game_dir: Option<String>,
-    pub map: Option<String>,
-    pub has_password: bool,
-    pub not_empty: bool,
-    pub not_full: bool,
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum Filter {
+    IsSecured(bool),
+    Map(String),
+    CanHavePassword(bool),
+    CanBeEmpty(bool),
+    CanBeFull(bool),
+    AppId(u32),
 }
 
-impl Filters {
-    pub(crate) fn to_bytes<'a>(self) -> &'a [u8] { &[0x00] }
+fn bool_as_char_u8(b: bool) -> u8 {
+    match b {
+        true => '1' as u8,
+        false => '0' as u8,
+    }
+}
+
+impl Filter {
+    pub(crate) fn to_bytes(self) -> Vec<u8> {
+        let mut bytes: Vec<u8>;
+
+        match self {
+            Filter::IsSecured(secured) => {
+                bytes = "\\secure\\".as_bytes().to_vec();
+                bytes.extend([bool_as_char_u8(secured)]);
+            }
+            Filter::Map(map) => {
+                bytes = "\\map\\".as_bytes().to_vec();
+                bytes.extend(map.as_bytes());
+            }
+            Filter::CanHavePassword(password) => {
+                bytes = "\\password\\".as_bytes().to_vec();
+                bytes.extend([bool_as_char_u8(password)]);
+            }
+            Filter::CanBeEmpty(empty) => {
+                bytes = "\\empty\\".as_bytes().to_vec();
+                bytes.extend([bool_as_char_u8(empty)]);
+            }
+            Filter::CanBeFull(full) => {
+                bytes = "\\full\\".as_bytes().to_vec();
+                bytes.extend([bool_as_char_u8(full)]);
+            }
+            Filter::AppId(id) => {
+                bytes = "\\appid\\".as_bytes().to_vec();
+                bytes.extend(id.to_string().as_bytes());
+            }
+        }
+
+        bytes.extend([0x00]);
+        bytes
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct SearchFilters {
+    filters: Vec<Filter>,
+}
+
+impl SearchFilters {
+    pub(crate) fn new() -> Self {
+        Self {
+            filters: Vec::new(),
+        }
+    }
+
+    pub(crate) fn add(self, filter: Filter) -> Self {
+        let mut last_filters = self.filters;
+
+        let found_same_filter = last_filters.iter_mut().find_map(|f| {
+            if std::mem::discriminant(f) == std::mem::discriminant(&filter) {
+                Some(f)
+            } else {
+                None
+            }
+        });
+
+        match found_same_filter {
+            None => last_filters.push(filter),
+            Some(f) => *f = filter,
+        }
+
+        Self {
+            filters: last_filters,
+        }
+    }
+
+    // pub(crate) fn to_bytes<'a>(self) -> &'a [u8] { &[0x00] }
+    pub(crate) fn to_bytes(self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
+
+        for filter in self.filters {
+            bytes.extend(filter.to_bytes())
+        }
+
+        bytes
+    }
 }
 
 #[derive(Eq, PartialEq, Copy, Clone)]
