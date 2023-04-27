bb0b7113288bcd3cb642569e080c005bd7e2ccb9
CosminPerRam <cosmin.p@live.com>
2023-04-27 19:18:33 +0300

    [Service] Add complete and singular query

--- src/services/valve_master_server/service.rs
+++ src/services/valve_master_server/service.rs
@@ -4,7 +4,10 @@ use crate::valve_master_server::{Region, SearchFilters};
 use crate::{GDError, GDResult};
 use std::net::Ipv4Addr;
 
-fn construct_payload(region: Region, filters: Option<SearchFilters>, last_ip: String, last_port: u16) -> Vec<u8> {
+pub const DEFAULT_MASTER_IP: &str = "hl2master.steampowered.com";
+pub const DEFAULT_MASTER_PORT: u16 = 27011;
+
+fn construct_payload(region: Region, filters: &Option<SearchFilters>, last_ip: &str, last_port: u16) -> Vec<u8> {
     let filters_bytes: Vec<u8> = match filters {
         None => vec![0x00],
         Some(f) => f.to_bytes(),
@@ -31,41 +34,107 @@ fn construct_payload(region: Region, filters: Option<SearchFilters>, last_ip: St
     .concat()
 }
 
+struct ValveMasterServer {
+    socket: UdpSocket,
+}
+
+impl ValveMasterServer {
+    pub fn new(master_ip: &str, master_port: u16) -> GDResult<Self> {
+        let socket = UdpSocket::new(master_ip, master_port)?;
+        socket.apply_timeout(None)?;
+
+        Ok(Self { socket })
+    }
+
+    pub fn query_specific(
+        &mut self,
+        region: Region,
+        search_filters: &Option<SearchFilters>,
+        last_address_ip: &str,
+        last_address_port: u16,
+    ) -> GDResult<Vec<(Ipv4Addr, u16)>> {
+        let initial_payload = construct_payload(region, search_filters, last_address_ip, last_address_port);
+        self.socket.send(&initial_payload)?;
+
+        let received_data = self.socket.receive(Some(1400))?;
+        let mut buf = Bufferer::new_with_data(Endianess::Big, &received_data);
+
+        if buf.get_u32()? != 4294967295 || buf.get_u16()? != 26122 {
+            return Err(GDError::PacketBad);
+        }
+
+        let mut ips: Vec<(Ipv4Addr, u16)> = Vec::new();
+        while buf.remaining_length() > 0 {
+            let ip = Ipv4Addr::new(buf.get_u8()?, buf.get_u8()?, buf.get_u8()?, buf.get_u8()?);
+            let port = buf.get_u16()?;
+
+            ips.push((ip, port));
+        }
+
+        Ok(ips)
+    }
+
+    pub fn query(&mut self, region: Region, search_filters: Option<SearchFilters>) -> GDResult<Vec<(Ipv4Addr, u16)>> {
+        let mut ips: Vec<(Ipv4Addr, u16)> = Vec::new();
+
+        let mut exit_fetching = false;
+        let mut last_ip: String = "0.0.0.0".to_string();
+        let mut last_port: u16 = 0;
+
+        while !exit_fetching {
+            let new_ips = self.query_specific(region, &search_filters, last_ip.as_str(), last_port)?;
+
+            match new_ips.last() {
+                None => exit_fetching = true,
+                Some((latest_ip, latest_port)) => {
+                    let mut remove_last = false;
+
+                    let latest_ip_string = latest_ip.to_string();
+                    if latest_ip_string == last_ip && *latest_port == last_port {
+                        exit_fetching = true;
+                    } else if latest_ip_string == "0.0.0.0" && *latest_port == 0 {
+                        exit_fetching = true;
+                        remove_last = true;
+                    } else {
+                        last_ip = latest_ip_string;
+                        last_port = *latest_port;
+                    }
+
+                    ips.extend(new_ips);
+                    if remove_last {
+                        ips.pop();
+                    }
+                }
+            }
+        }
+
+        Ok(ips)
+    }
+}
+
+pub fn query_singular(region: Region, search_filters: Option<SearchFilters>) -> GDResult<Vec<(Ipv4Addr, u16)>> {
+    let mut master_server = ValveMasterServer::new(DEFAULT_MASTER_IP, DEFAULT_MASTER_PORT)?;
+
+    master_server.query_specific(region, &search_filters, "0.0.0.0", 0)
+}
+
 pub fn query(region: Region, search_filters: Option<SearchFilters>) -> GDResult<Vec<(Ipv4Addr, u16)>> {
-    let mut socket = UdpSocket::new("hl2master.steampowered.com", 27011)?;
-    socket.apply_timeout(None)?;
+    let mut master_server = ValveMasterServer::new(DEFAULT_MASTER_IP, DEFAULT_MASTER_PORT)?;
 
-    let initial_payload = construct_payload(region, search_filters, "0.0.0.0".to_string(), 0);
-    socket.send(&initial_payload)?;
-
-    let received_data = socket.receive(Some(1400))?;
-    let mut buf = Bufferer::new_with_data(Endianess::Big, &received_data);
-
-    if buf.get_u32()? != 4294967295 || buf.get_u16()? != 26122 {
-        return Err(GDError::PacketBad);
-    }
-
-    let mut ips: Vec<(Ipv4Addr, u16)> = Vec::new();
-    while buf.remaining_length() > 0 {
-        let ip = Ipv4Addr::new(buf.get_u8()?, buf.get_u8()?, buf.get_u8()?, buf.get_u8()?);
-        let port = buf.get_u16()?;
-
-        ips.push((ip, port));
-    }
-
-    Ok(ips)
+    master_server.query(region, search_filters)
 }
 
 #[cfg(test)]
 mod master_query {
-    use crate::valve_master_server::{query, Filter, Region, SearchFilters};
+    use crate::valve_master_server::{query, query_singular, Filter, Region, SearchFilters};
 
     #[test]
     fn test_stuff() {
         let search_filters = SearchFilters::new()
             .add(Filter::AppId(440))
-            .add(Filter::CanHavePassword(false));
+            .add(Filter::CanHavePassword(true));
 
-        println!("{:?}", query(Region::Europe, Some(search_filters)).unwrap());
+        let ips = query(Region::Europe, Some(search_filters)).unwrap();
+        println!("{:?} {}", ips, ips.len());
     }
 }
