a17f5ad4d2e34091e233a32061b91d4fac2a5f8b
CosminPerRam <cosmin.p@live.com>
2023-05-08 14:59:55 +0300

    [Service] Add merge methods on SearchFilters

--- src/services/valve_master_server/types.rs
+++ src/services/valve_master_server/types.rs
@@ -154,6 +154,28 @@ impl<'a> SearchFilters<'a> {
         }
     }
 
+    pub fn merge_all(self, others: SearchFilters<'a>) -> Self {
+        let mut updated = self.merge_normals(&others);
+        updated = updated.merge_nors(&others);
+        updated = updated.merge_nands(&others);
+
+        updated
+    }
+
+    pub fn merge_normals(self, others: &SearchFilters<'a>) -> Self {
+        let mut updated_fitler = self.filters;
+
+        for (filter_key, filter_value) in &others.filters {
+            updated_fitler.insert(*filter_key, *filter_value);
+        }
+
+        Self {
+            filters: updated_fitler,
+            nand_filters: self.nand_filters,
+            nor_filters: self.nor_filters,
+        }
+    }
+
     pub fn insert(self, filter: Filter<'a>) -> Self {
         let mut updated_fitler = self.filters;
         updated_fitler.insert(std::mem::discriminant(&filter), filter);
@@ -165,9 +187,37 @@ impl<'a> SearchFilters<'a> {
         }
     }
 
+    pub fn merge_nands(self, others: &SearchFilters<'a>) -> Self {
+        let mut updated_fitler = self.nand_filters;
+
+        for (filter_key, filter_value) in &others.nand_filters {
+            updated_fitler.insert(*filter_key, *filter_value);
+        }
+
+        Self {
+            filters: self.filters,
+            nand_filters: updated_fitler,
+            nor_filters: self.nor_filters,
+        }
+    }
+
     pub fn insert_nand(self, filter: Filter<'a>) -> Self {
         let mut updated_fitler = self.nor_filters;
         updated_fitler.insert(std::mem::discriminant(&filter), filter);
+
+        Self {
+            filters: self.filters,
+            nand_filters: self.nand_filters,
+            nor_filters: updated_fitler,
+        }
+    }
+
+    pub fn merge_nors(self, others: &SearchFilters<'a>) -> Self {
+        let mut updated_fitler = self.nor_filters;
+
+        for (filter_key, filter_value) in &others.nor_filters {
+            updated_fitler.insert(*filter_key, *filter_value);
+        }
 
         Self {
             filters: self.filters,
@@ -229,4 +279,27 @@ pub enum Region {
     MiddleEast = 0x06,
     Africa = 0x07,
     Others = 0xFF,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::valve_master_server::{Filter, SearchFilters};
+
+    #[test]
+    fn merge_normals_test() {
+        let filters_a = SearchFilters::new()
+            .insert(Filter::IsSecured(true));
+
+        let filters_b = SearchFilters::new()
+            .insert(Filter::CanBeFull(true))
+            .insert(Filter::IsSecured(false));
+
+        let combined = filters_a.merge_all(filters_b);
+
+        let composed = SearchFilters::new()
+            .insert(Filter::IsSecured(false))
+            .insert(Filter::CanBeFull(true));
+
+        assert_eq!(combined, composed)
+    }
 }
