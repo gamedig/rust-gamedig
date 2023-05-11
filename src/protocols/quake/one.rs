cdd6ed0b8b557c88c634f6002f68ae5f4466a4fe
CosminPerRam <cosmin.p@live.com>
2023-05-11 20:23:07 +0300

    [Protocol] Add key extraction

--- src/protocols/quake/one.rs
+++ src/protocols/quake/one.rs
@@ -26,15 +26,32 @@ fn get_server_values(
         return Err(GDError::PacketBad);
     }
 
-    println!("{:02X?}", bufferer.remaining_data());
-    println!("{}", bufferer.get_string_utf8()?);
+    let data = bufferer.get_string_utf8()?;
+    let after_the_first_weird_value = data.split("\\")
+        .into_iter()
+        .skip(1)
+        .collect::<Vec<&str>>();
+    let values = after_the_first_weird_value.chunks(2);
 
-    Ok(HashMap::new())
+    let mut vars: HashMap<String, String> = HashMap::new();
+    for data in values {
+        let key = data.get(0);
+        let value = data.get(1);
+
+        if let Some(k) = key {
+            if let Some(v) = value {
+                vars.insert(k.to_string(), v.to_string());
+            }
+        }
+    }
+
+    Ok(vars)
 }
 
 pub fn query(address: &str, port: u16, timeout_settings: Option<TimeoutSettings>) -> GDResult<Response> {
     let server_vars = get_server_values(address, port, timeout_settings)?;
 
+    println!("{:#?}", server_vars);
     Ok(Response {
         name: "test".to_string()
     })
