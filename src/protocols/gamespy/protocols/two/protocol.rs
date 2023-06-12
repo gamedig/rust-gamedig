a5daf04eb62caea3e892e2adcf2ebaec4b3b6243
CosminPerRam <cosmin.p@live.com>
2023-06-12 17:44:22 +0300

    [Protocol] Parse teams

--- src/protocols/gamespy/protocols/two/protocol.rs
+++ src/protocols/gamespy/protocols/two/protocol.rs
@@ -1,5 +1,5 @@
 use crate::bufferer::{Bufferer, Endianess};
-use crate::protocols::gamespy::two::Response;
+use crate::protocols::gamespy::two::{Response, Team};
 use crate::protocols::types::TimeoutSettings;
 use crate::socket::{Socket, UdpSocket};
 use crate::{GDError, GDResult};
@@ -18,14 +18,52 @@ enum RequestType {
 }
 
 impl RequestType {
-    pub fn to_bytes(&self) -> Vec<u8> {
+    pub fn to_bytes(&self) -> [u8; 3] {
         match self {
-            RequestType::INFO => vec![0xFF, 0x00, 0x00],
-            RequestType::PLAYERS => vec![0x00, 0xFF, 0x00],
-            RequestType::TEAMS => vec![0x00, 0x00, 0xFF],
-            RequestType::ALL => vec![0xFF, 0xFF, 0xFF],
+            RequestType::INFO => [0xFF, 0x00, 0x00],
+            RequestType::PLAYERS => [0x00, 0xFF, 0x00],
+            RequestType::TEAMS => [0x00, 0x00, 0xFF],
+            RequestType::ALL => [0xFF, 0xFF, 0xFF],
         }
     }
+}
+
+fn data_as_table(mut data: Bufferer) -> GDResult<(HashMap<String, Vec<String>>, usize)> {
+    if data.get_u8()? != 0 {
+        Err(GDError::PacketBad)?
+    }
+
+    let rows = data.get_u8()? as usize;
+
+    if rows == 0 {
+        return Ok((HashMap::new(), 0));
+    }
+
+    let mut column_heads = Vec::new();
+
+    let mut current_column = data.get_string_utf8()?;
+    while !current_column.is_empty() {
+        column_heads.push(current_column.clone());
+        current_column = data.get_string_utf8()?;
+    }
+
+    let columns = column_heads.len();
+    let mut table = HashMap::with_capacity(columns);
+    for head in &column_heads {
+        table.insert(head.clone(), Vec::new());
+    }
+
+    for _ in 0 .. rows {
+        for column_index in 0 .. columns {
+            let value = data.get_string_utf8()?;
+            table
+                .get_mut(&*column_heads[column_index])
+                .ok_or(GDError::PacketBad)?
+                .push(value);
+        }
+    }
+
+    Ok((table, rows))
 }
 
 impl GameSpy2 {
@@ -41,7 +79,7 @@ impl GameSpy2 {
             &*[
                 vec![0xFE, 0xFD, 0x00],
                 vec![0x00, 0x00, 0x00, 0x01],
-                request.to_bytes(),
+                request.to_bytes().to_vec(),
             ]
             .concat(),
         )?;
@@ -64,7 +102,6 @@ impl GameSpy2 {
         let mut values = HashMap::new();
 
         let mut data = self.request(RequestType::INFO)?;
-
         while data.remaining_length() > 0 {
             let key = data.get_string_utf8()?;
             let value = data.get_string_utf8_optional()?;
@@ -77,6 +114,33 @@ impl GameSpy2 {
         }
 
         Ok(values)
+    }
+
+    fn get_teams(&mut self) -> GDResult<Vec<Team>> {
+        let mut teams = Vec::new();
+
+        let data = self.request(RequestType::TEAMS)?;
+        let (table, entries) = data_as_table(data)?;
+
+        for index in 0 .. entries {
+            teams.push(Team {
+                name: table
+                    .get("team_t")
+                    .ok_or(GDError::PacketBad)?
+                    .get(index)
+                    .ok_or(GDError::PacketBad)?
+                    .clone(),
+                score: table
+                    .get("score_t")
+                    .ok_or(GDError::PacketBad)?
+                    .get(index)
+                    .ok_or(GDError::PacketBad)?
+                    .parse()
+                    .map_err(|_| GDError::PacketBad)?,
+            })
+        }
+
+        Ok(teams)
     }
 }
 
@@ -93,5 +157,6 @@ pub fn query(address: &SocketAddr, timeout_settings: Option<TimeoutSettings>) ->
             .ok_or(GDError::PacketBad)?
             .parse()
             .map_err(|_| GDError::PacketBad)?,
+        teams: client.get_teams()?,
     })
 }
--- src/protocols/gamespy/protocols/two/types.rs
+++ src/protocols/gamespy/protocols/two/types.rs
@@ -1,7 +1,14 @@
 #[derive(Debug)]
+pub struct Team {
+    pub name: String,
+    pub score: u16,
+}
+
+#[derive(Debug)]
 pub struct Response {
     pub name: String,
     pub map: String,
     pub has_password: bool,
     pub max_players: u8,
+    pub teams: Vec<Team>,
 }
