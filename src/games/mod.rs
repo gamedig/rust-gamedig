685dd3da55617ccae7c24b9de443ff5550b6e421
Douile <douile@douile.com>
2023-09-21 21:00:30 +0100

    Update game file names to match definitions

--- src/games/mod.rs
+++ src/games/mod.rs
@@ -3,34 +3,36 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-/// Alien Swarm
-pub mod aliens;
 /// Age of Chivalry
-pub mod aoc;
+pub mod ageofchivalry;
+/// Alien Swarm
+pub mod alienswarm;
+/// ARK: Survival Evolved
+pub mod arkse;
 /// ARMA 2: Operation Arrowhead
 pub mod arma2oa;
-/// ARK: Survival Evolved
-pub mod ase;
 /// Alien Swarm: Reactive Drop
 pub mod asrd;
 /// Avorion
 pub mod avorion;
+/// Ballistic Overkill
+pub mod ballisticoverkill;
 /// Battalion 1944
 pub mod bat1944;
-/// BrainBread 2
-pub mod bb2;
 /// Battlefield 1942
 pub mod bf1942;
 /// Black Mesa
-pub mod bm;
-/// Ballistic Overkill
-pub mod bo;
+pub mod blackmesa;
+/// BrainBread 2
+pub mod brainbread2;
 /// Codename CURE
-pub mod ccure;
+pub mod codenamecure;
 /// Colony Survival
-pub mod cosu;
+pub mod colonysurvival;
 /// Creativerse
 pub mod creativerse;
+/// Crysis Wars
+pub mod crysiswars;
 /// Counter-Strike
 pub mod cs;
 /// Counter Strike: Condition Zero
@@ -39,8 +41,6 @@ pub mod cscz;
 pub mod csgo;
 /// Counter-Strike: Source
 pub mod css;
-/// Crysis Wars
-pub mod cw;
 /// Day of Defeat
 pub mod dod;
 /// Day of Defeat: Source
@@ -52,7 +52,7 @@ pub mod dst;
 /// Frontlines: Fuel of War
 pub mod ffow;
 /// Garry's Mod
-pub mod gm;
+pub mod garrysmod;
 /// Halo: Combat Evolved
 pub mod haloce;
 /// Half-Life 2 Deathmatch
@@ -62,17 +62,17 @@ pub mod hldms;
 /// Hell Let Loose
 pub mod hll;
 /// Insurgency
-pub mod ins;
+pub mod insurgency;
 /// Insurgency: Modern Infantry Combat
-pub mod insmic;
+pub mod insurgencymic;
 /// Insurgency: Sandstorm
-pub mod inss;
+pub mod insurgencysandstorm;
 /// Just Cause 2: Multiplayer
 pub mod jc2mp;
 /// Left 4 Dead
-pub mod l4d;
+pub mod left4dead;
 /// Left 4 Dead 2
-pub mod l4d2;
+pub mod left4dead2;
 /// Minecraft
 pub mod mc;
 /// Operation: Harsh Doorstop
@@ -80,39 +80,39 @@ pub mod ohd;
 /// Onset
 pub mod onset;
 /// Project Zomboid
-pub mod pz;
+pub mod przomboid;
 /// Quake 1
 pub mod quake1;
 /// Quake 2
 pub mod quake2;
 /// Quake 3: Arena
-pub mod quake3a;
+pub mod quake3;
 /// Risk of Rain 2
 pub mod ror2;
 /// Rust
 pub mod rust;
-/// Sven Co-op
-pub mod sc;
 /// 7 Days To Die
-pub mod sdtd;
+pub mod sd2d;
+/// The Ship
+pub mod ship;
 /// Soldier of Fortune 2
 pub mod sof2;
 /// Serious Sam
 pub mod ss;
+/// Sven Co-op
+pub mod svencoop;
 /// The Forest
 pub mod tf;
 /// Team Fortress 2
 pub mod tf2;
 /// Team Fortress Classic
 pub mod tfc;
-/// The Ship
-pub mod ts;
 /// Unturned
 pub mod unturned;
 /// Unreal Tournament
 pub mod ut;
 /// V Rising
-pub mod vr;
+pub mod vrising;
 /// Warsow
 pub mod warsow;
 
@@ -222,7 +222,7 @@ pub fn query_with_timeout_and_extra_settings(
         Protocol::PROPRIETARY(protocol) => {
             match protocol {
                 ProprietaryProtocol::TheShip => {
-                    ts::query_with_timeout(address, port, timeout_settings).map(Box::new)?
+                    ship::query_with_timeout(address, port, timeout_settings).map(Box::new)?
                 }
                 ProprietaryProtocol::FFOW => ffow::query_with_timeout(address, port, timeout_settings).map(Box::new)?,
                 ProprietaryProtocol::JC2MP => {
