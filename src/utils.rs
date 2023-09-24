ecf0f2c4730658c33d125d1943728d08f3039b69
Douile <douile@douile.com>
2023-09-24 16:21:00 +0100

    Also retry on PacketSend error
    Sending packets could also timeout and until error_generic_member_access
    is stable we have no way of determining the type of the underlying
    `std::error::Error`.

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use crate::GDErrorKind::{PacketOverflow, PacketReceive, PacketUnderflow};
+use crate::GDErrorKind::{PacketOverflow, PacketReceive, PacketSend, PacketUnderflow};
 use crate::GDResult;
 use std::cmp::Ordering;
 
@@ -12,13 +12,16 @@ pub fn error_by_expected_size(expected: usize, size: usize) -> GDResult<()> {
 
 pub const fn u8_lower_upper(n: u8) -> (u8, u8) { (n & 15, n >> 4) }
 
+/// Run a closure `retry_count+1` times while it returns [PacketReceive] or
+/// [PacketSend] errors, returning the first success, other Error, or after
+/// `retry_count+1` tries the last [PacketReceive] or [PacketSend] error.
 pub fn retry_on_timeout<T>(mut retry_count: usize, mut fetch: impl FnMut() -> GDResult<T>) -> GDResult<T> {
     let mut last_err = PacketReceive.context("Retry count was 0");
     retry_count += 1;
     while retry_count > 0 {
         last_err = match fetch() {
             Ok(r) => return Ok(r),
-            Err(e) if e.kind == PacketReceive => e,
+            Err(e) if e.kind == PacketReceive || e.kind == PacketSend => e,
             Err(e) => return Err(e),
         };
         retry_count -= 1;
