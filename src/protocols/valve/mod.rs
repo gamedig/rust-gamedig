34f6b27d4c5e133ae8a98fb75f677d35013e3d3c
CosminPerRam <cosmin.p@live.com>
2023-10-17 21:13:09 +0300

    Initial macro modification for gathering settings

--- src/protocols/valve/mod.rs
+++ src/protocols/valve/mod.rs
@@ -39,17 +39,17 @@ macro_rules! game_query_fn {
     ($steam_app: ident, $default_port: literal) => {
         crate::protocols::valve::game_query_fn!{@gen $steam_app, $default_port, concat!(
             "Make a valve query for ", stringify!($steam_app), " with default timeout settings and default extra request settings.\n\n",
-            "If port is `None`, then the default port (", stringify!($default_port), ") will be used.")}
+            "If port is `None`, then the default port (", stringify!($default_port), ") will be used."), None}
     };
 
-    (@gen $steam_app: ident, $default_port: literal, $doc: expr) => {
+    (@gen $steam_app: ident, $default_port: literal, $doc: expr, $gathering_settings: expr) => {
         #[doc = $doc]
         pub fn query(address: &std::net::IpAddr, port: Option<u16>) -> crate::GDResult<crate::protocols::valve::game::Response> {
             let valve_response = crate::protocols::valve::query(
                 &std::net::SocketAddr::new(*address, port.unwrap_or($default_port)),
                 crate::protocols::valve::SteamApp::$steam_app.as_engine(),
                 None,
-                None,
+                $gathering_settings,
             )?;
 
             Ok(crate::protocols::valve::game::Response::new_from_valve_response(valve_response))
