dc252cdc6329191be04092d4e1a211fb2c8082a9
Douile <douile@douile.com>
2023-11-23 12:43:06 +0000

    cli: Refactor IP resolution into its own set of functions
    Co-Authored-By: Cain <75994858+cainthebest@users.noreply.github.com>

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,4 @@
-use std::net::ToSocketAddrs;
+use std::net::{IpAddr, ToSocketAddrs};
 
 use clap::Parser;
 use gamedig::{
@@ -53,6 +53,63 @@ fn find_game(game_id: &str) -> Result<&'static Game> {
         .ok_or_else(|| Error::UnknownGame(game_id.to_string()))
 }
 
+/// Resolve an IP address by either parsing an IP address or doing a DNS lookup.
+/// In the case of DNS lookup update extra request options with the hostname.
+///
+/// # Arguments
+/// * `host` - A string slice containing the IP address or hostname of a server
+///   to resolve.
+/// * `extra_options` - Mutable reference to extra options for the game query.
+///
+/// # Returns
+/// * `Result<IpAddr>` - On sucess returns a resolved IP address; on failure
+///   returns an [Error::InvalidHostname] error.
+fn resolve_ip_or_domain(host: &str, extra_options: &mut Option<ExtraRequestSettings>) -> Result<IpAddr> {
+    if let Ok(parsed_ip) = host.parse() {
+        Ok(parsed_ip)
+    } else {
+        set_hostname_if_missing(host, extra_options);
+
+        resolve_domain(host)
+    }
+}
+
+/// Resolve a domain name to one of its IP addresses (the first one returned).
+///
+/// # Arguments
+/// * `domain` - A string slice containing the domain name to lookup.
+///
+/// # Returns
+/// * `Result<IpAddr>` - On success, returns one of the resolved IP addresses;
+///   on failure returns an [Error::InvalidHostname] error.
+fn resolve_domain(domain: &str) -> Result<IpAddr> {
+    // Append a dummy port to perform socket address resolution and then extract the
+    // IP
+    Ok(format!("{}:0", domain)
+        .to_socket_addrs()
+        .map_err(|_| Error::InvalidHostname(domain.to_string()))?
+        .next()
+        .ok_or_else(|| Error::InvalidHostname(domain.to_string()))?
+        .ip())
+}
+
+/// Sets the hostname on extra request settings if it is not already set.
+///
+/// # Arguments
+/// * `host` - A string slice containing the hostname.
+/// * `extra_options` - A mutable reference to optional [ExtraRequestSettings].
+fn set_hostname_if_missing(host: &str, extra_options: &mut Option<ExtraRequestSettings>) {
+    if let Some(extra_options) = extra_options {
+        if extra_options.hostname.is_none() {
+            // If extra_options exists but hostname is None overwrite hostname in place
+            extra_options.hostname = Some(host.to_string())
+        }
+    } else {
+        // If extra_options is None create default settings with hostname
+        *extra_options = Some(ExtraRequestSettings::default().set_hostname(host.to_string()));
+    }
+}
+
 fn main() -> Result<()> {
     // Parse the command line arguments
     let args = Cli::parse();
@@ -60,37 +117,13 @@ fn main() -> Result<()> {
     // Retrieve the game based on the provided ID
     let game = find_game(&args.game)?;
 
-    let mut extra_request_settings = if let Some(extra) = args.extra_options {
-        extra
-    } else {
-        gamedig::protocols::ExtraRequestSettings::default()
-    };
+    // Extract extra options for use in setup
+    let mut extra_options = args.extra_options;
 
-    let ip = if let Ok(ip) = args.ip.parse() {
-        ip
-    } else {
-        // Set hostname in extra request settings
-        if extra_request_settings.hostname.is_none() {
-            extra_request_settings.hostname = Some(args.ip.clone());
-        }
+    // Resolve the IP address
+    let ip = resolve_ip_or_domain(&args.ip, &mut extra_options)?;
 
-        // Use ToSocketAddrs to do a DNS lookup
-        // unfortunatley this requires a format to add a port
-        format!("{}:0", args.ip)
-            .to_socket_addrs()
-            .map_err(|_| error::Error::InvalidHostname(args.ip.clone()))?
-            .next()
-            .ok_or(error::Error::InvalidHostname(args.ip))?
-            .ip()
-    };
-
-    let result = query_with_timeout_and_extra_settings(
-        game,
-        &ip,
-        args.port,
-        args.timeout_settings,
-        Some(extra_request_settings),
-    )?;
+    let result = query_with_timeout_and_extra_settings(game, &ip, args.port, args.timeout_settings, extra_options)?;
 
     #[cfg(feature = "json")]
     if args.json {
