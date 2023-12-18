cc08bea21100d8a763064e7ff388bdabba631793
CosminPerRam <cosmin.p@live.com>
2023-12-18 13:56:16 +0100

    fix: actual the ship reference link

--- crates/lib/src/games/savage2/mod.rs
+++ crates/lib/src/games/savage2/mod.rs
@@ -1,5 +1,5 @@
 /// The implementation.
-/// Reference: [Node-GameGig](https://github.com/gamedig/node-gamedig/blob/master/protocols/savage2.js)
+/// Reference: [server queries](https://developer.valvesoftware.com/wiki/Server_queries)
 pub mod protocol;
 /// All types used by the implementation.
 pub mod types;
