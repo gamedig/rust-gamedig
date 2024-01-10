3829d4afe99468f7b4e3f1f601f5ac45b5ef6321
Douile <douile@douile.com>
2024-01-10 19:44:41 +0000

    id-tests: Add CLI that reads JSON input

---
+++ crates/id-tests/src/main.rs
@@ -0,0 +1,40 @@
+#![cfg(feature = "cli")]
+
+use std::collections::HashMap;
+
+/// Format for input games (the same as used in node-gamedig/lib/games.js).
+type GamesInput = HashMap<String, Game>;
+
+#[derive(Debug, Clone, PartialEq, serde::Deserialize)]
+struct Game {
+    name: String,
+    options: Option<GameOptions>,
+}
+
+#[derive(Debug, Clone, PartialEq, serde::Deserialize)]
+struct GameOptions {
+    port: Option<u16>,
+    protocol: Option<String>,
+    port_query: Option<u16>,
+    port_query_offset: Option<u16>,
+}
+
+use gamedig_id_tests::test_game_name_rules;
+
+fn main() {
+    let games: GamesInput = if let Some(file) = std::env::args_os().skip(1).next() {
+        let file = std::fs::OpenOptions::new().read(true).open(file).unwrap();
+
+        serde_json::from_reader(file).unwrap()
+    } else {
+        serde_json::from_reader(std::io::stdin().lock()).unwrap()
+    };
+
+    let failed = test_game_name_rules(
+        games
+            .iter()
+            .map(|(key, game)| (key.as_str(), game.name.as_str())),
+    );
+
+    assert!(failed.is_empty());
+}
