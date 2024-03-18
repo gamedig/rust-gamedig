775328acb5524958035b6adb9e39ec4316b540e3
CosminPerRam <cosmin.p@live.com>
2024-03-18 02:07:21 +0200

    feat: add initial generic impl

--- crates/lib/src/protocols/epic/types.rs
+++ crates/lib/src/protocols/epic/types.rs
@@ -1,6 +1,9 @@
+use crate::protocols::types::{CommonPlayer, CommonResponse, GenericPlayer};
+use crate::protocols::GenericResponse;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Response {
     pub name: String,
     pub map: String,
@@ -11,7 +14,33 @@ pub struct Response {
     pub raw: Value,
 }
 
-#[derive(Debug)]
+impl CommonResponse for Response {
+    fn as_original(&self) -> GenericResponse { GenericResponse::Epic(self) }
+    fn name(&self) -> Option<&str> { Some(&self.name) }
+    fn map(&self) -> Option<&str> { Some(&self.map) }
+    fn players_maximum(&self) -> u32 { self.players_maxmimum }
+
+    fn players_online(&self) -> u32 { self.players_online }
+
+    fn has_password(&self) -> Option<bool> { Some(self.has_password) }
+
+    fn players(&self) -> Option<Vec<&dyn CommonPlayer>> {
+        Some(
+            self.players
+                .iter()
+                .map(|p| p as &dyn CommonPlayer)
+                .collect(),
+        )
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Player {
     pub name: String,
+}
+
+impl CommonPlayer for Player {
+    fn as_original(&self) -> GenericPlayer { GenericPlayer::Epic(self) }
+
+    fn name(&self) -> &str { &self.name }
 }
