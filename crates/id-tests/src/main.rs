f53635993e3900e60f4678ed53c704ff1c29c31a
CosminPerRam <cosmin.p@live.com>
2024-05-01 18:44:21 +0300

    feat: use map_or_else instead of if else on option

--- crates/id-tests/src/main.rs
+++ crates/id-tests/src/main.rs
@@ -13,13 +13,14 @@ struct Game {
 use gamedig_id_tests::test_game_name_rules;
 
 fn main() {
-    let games: GamesInput = if let Some(file) = std::env::args_os().nth(1) {
-        let file = std::fs::OpenOptions::new().read(true).open(file).unwrap();
+    let games: GamesInput = std::env::args_os().nth(1).map_or_else(
+        || serde_json::from_reader(std::io::stdin().lock()).unwrap(),
+        |file| {
+            let file = std::fs::OpenOptions::new().read(true).open(file).unwrap();
 
-        serde_json::from_reader(file).unwrap()
-    } else {
-        serde_json::from_reader(std::io::stdin().lock()).unwrap()
-    };
+            serde_json::from_reader(file).unwrap()
+        },
+    );
 
     let failed = test_game_name_rules(
         games
