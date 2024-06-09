0ebe9c875df3e0d91fc5080cecbd386c54e468c7
Cain <75994858+cainthebest@users.noreply.github.com>
2024-06-09 23:19:11 +0100

    refactor: clean up timeout settings

--- crates/lib/src/settings.rs
+++ crates/lib/src/settings.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+
 pub struct Timeout {
     pub connect: Duration,
     pub read: Duration,
@@ -10,6 +11,12 @@ pub struct Timeout {
 impl Timeout {
     pub const DEFAULT_DURATION: Duration = Duration::from_secs(5);
     pub const DEFAULT_RETRIES: u8 = 0;
+    pub const DEFAULT: Self = Self {
+        connect: Self::DEFAULT_DURATION,
+        read: Self::DEFAULT_DURATION,
+        write: Self::DEFAULT_DURATION,
+        retries: Self::DEFAULT_RETRIES,
+    };
 
     pub const fn new(
         connect: Option<Duration>,
@@ -19,15 +26,15 @@ impl Timeout {
     ) -> Self {
         Self {
             connect: match connect {
-                Some(d) if Self::is_non_zero(d) => d,
+                Some(d) if !d.is_zero() => d,
                 _ => Self::DEFAULT_DURATION,
             },
             read: match read {
-                Some(d) if Self::is_non_zero(d) => d,
+                Some(d) if !d.is_zero() => d,
                 _ => Self::DEFAULT_DURATION,
             },
             write: match write {
-                Some(d) if Self::is_non_zero(d) => d,
+                Some(d) if !d.is_zero() => d,
                 _ => Self::DEFAULT_DURATION,
             },
             retries: match retries {
@@ -36,21 +43,8 @@ impl Timeout {
             },
         }
     }
-
-    const fn is_non_zero(duration: Duration) -> bool {
-        duration.as_secs() != 0 || duration.subsec_nanos() != 0
-    }
-
-    const fn const_default() -> Self {
-        Self {
-            connect: Self::DEFAULT_DURATION,
-            read: Self::DEFAULT_DURATION,
-            write: Self::DEFAULT_DURATION,
-            retries: Self::DEFAULT_RETRIES,
-        }
-    }
 }
 
 impl Default for Timeout {
-    fn default() -> Self { Self::const_default() }
+    fn default() -> Self { Self::DEFAULT }
 }
