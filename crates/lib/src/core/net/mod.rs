2fc4564d2b775a6d554e79f0e15a4a3cdeaba693
Cain <75994858+cainthebest@users.noreply.github.com>
2024-06-10 19:58:27 +0100

    chore: add feature flags for core net

--- crates/lib/src/core/net/mod.rs
+++ crates/lib/src/core/net/mod.rs
@@ -1,4 +1,8 @@
+#[cfg(feature = "http")]
 pub(crate) mod http;
+#[cfg(feature = "rcon")]
 pub(crate) mod rcon;
+#[cfg(feature = "tcp")]
 pub(crate) mod tcp;
+#[cfg(feature = "udp")]
 pub(crate) mod udp;
