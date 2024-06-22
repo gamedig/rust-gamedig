0b6f9893abb69fe81c54d4953c29914996154986
CosminPerRam <cosmin.p@live.com>
2024-06-22 05:16:17 +0300

    restore tf2 example

--- crates/lib/examples/teamfortress2.rs
+++ crates/lib/examples/teamfortress2.rs
@@ -1,8 +1,12 @@
-use gamedig::{minetest_master_server, TimeoutSettings};
+use gamedig::games::teamfortress2;
 
 fn main() {
-    println!(
-        "{:#?}",
-        minetest_master_server::query(TimeoutSettings::default())
-    )
+    let response = teamfortress2::query(&"127.0.0.1".parse().unwrap(), None);
+    // or Some(27015), None is the default protocol port (which is 27015)
+
+    match response {
+        // Result type, must check what it is...
+        Err(error) => println!("Couldn't query, error: {}", error),
+        Ok(r) => println!("{:#?}", r),
+    }
 }
