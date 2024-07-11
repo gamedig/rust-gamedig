07aae4db53b67aa17d7b982b1f8f89fefa50615a
Cain <75994858+cainthebest@users.noreply.github.com>
2024-07-11 09:56:47 +0100

    refactor(lib core): add new errors to sync std tcp client

--- crates/lib/src/core/net/tcp/sealed/client/sync_std.rs
+++ crates/lib/src/core/net/tcp/sealed/client/sync_std.rs
@@ -1,12 +1,18 @@
 use std::{
-    fmt::{self, Display, Formatter},
     io::{Read, Write},
     net::{SocketAddr, TcpStream},
 };
 
-use error_stack::{Context, Report, Result, ResultExt};
-
-use crate::settings::Timeout;
+use crate::{
+    error::{
+        NetworkError,
+        Report,
+        Result,
+        ResultExt,
+        _metadata::{NetworkInterface, NetworkProtocol},
+    },
+    settings::Timeout,
+};
 
 #[derive(Debug)]
 pub(crate) struct SyncStdTcpClient {
@@ -15,60 +21,77 @@ pub(crate) struct SyncStdTcpClient {
 
 #[maybe_async::sync_impl]
 impl super::Tcp for SyncStdTcpClient {
-    type Error = SyncStdTcpClientError;
-
-    fn new(addr: &SocketAddr, timeout: &Timeout) -> Result<Self, SyncStdTcpClientError> {
+    fn new(addr: &SocketAddr, timeout: &Timeout) -> Result<Self> {
         let stream = TcpStream::connect_timeout(addr, timeout.connect)
             .map_err(Report::from)
             .attach_printable("Failed to establish a TCP connection")
             .attach_printable(format!("Attempted to connect to address: {addr:?}"))
-            .change_context(SyncStdTcpClientError)?;
+            .change_context(
+                NetworkError::ConnectionError {
+                    _protocol: NetworkProtocol::Tcp,
+                    _interface: NetworkInterface::SealedClientStd,
+                }
+                .into(),
+            )?;
 
         stream
             .set_read_timeout(Some(timeout.read))
             .map_err(Report::from)
             .attach_printable("Failed to set read timeout")
-            .change_context(SyncStdTcpClientError)?;
+            .change_context(
+                NetworkError::SetTimeoutError {
+                    _protocol: NetworkProtocol::Tcp,
+                    _interface: NetworkInterface::SealedClientStd,
+                }
+                .into(),
+            )?;
 
         stream
             .set_write_timeout(Some(timeout.write))
             .map_err(Report::from)
             .attach_printable("Failed to set write timeout")
-            .change_context(SyncStdTcpClientError)?;
+            .change_context(
+                NetworkError::SetTimeoutError {
+                    _protocol: NetworkProtocol::Tcp,
+                    _interface: NetworkInterface::SealedClientStd,
+                }
+                .into(),
+            )?;
 
         Ok(Self { stream })
     }
 
-    fn read(&mut self, size: Option<usize>) -> Result<Vec<u8>, SyncStdTcpClientError> {
+    fn read(&mut self, size: Option<usize>) -> Result<Vec<u8>> {
         let mut vec = Vec::with_capacity(size.unwrap_or(Self::DEFAULT_PACKET_SIZE as usize));
 
         self.stream
             .read_to_end(&mut vec)
             .map_err(Report::from)
             .attach_printable("Failed to read data from the TCP stream")
-            .change_context(SyncStdTcpClientError)?;
+            .change_context(
+                NetworkError::ReadError {
+                    _protocol: NetworkProtocol::Tcp,
+                    _interface: NetworkInterface::SealedClientStd,
+                }
+                .into(),
+            )?;
 
         Ok(vec)
     }
 
-    fn write(&mut self, data: &[u8]) -> Result<(), SyncStdTcpClientError> {
+    fn write(&mut self, data: &[u8]) -> Result<()> {
         self.stream
             .write_all(data)
             .map_err(Report::from)
             .attach_printable("Failed to write data to the TCP stream")
-            .change_context(SyncStdTcpClientError)?;
+            .change_context(
+                NetworkError::WriteError {
+                    _protocol: NetworkProtocol::Tcp,
+                    _interface: NetworkInterface::SealedClientStd,
+                }
+                .into(),
+            )?;
 
         Ok(())
-    }
-}
-
-#[derive(Debug)]
-pub struct SyncStdTcpClientError;
-
-impl Context for SyncStdTcpClientError {}
-
-impl Display for SyncStdTcpClientError {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result {
-        write!(fmt, "GameDig Core Net Runtime Error (sync_std_tcp_client)")
     }
 }
