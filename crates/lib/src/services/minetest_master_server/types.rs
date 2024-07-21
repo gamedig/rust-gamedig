41a3d88fb591f90356dbc655939585caeef13190
CosminPerRam <cosmin.p@live.com>
2024-07-21 03:01:10 +0300

    feat: add minetest master server service (#209)
    * feat: add minetest master server service"
    
    * restore tf2 example
    
    * chore: replace default with None
    
    * fix: make it available only on TLS and serde
    
    * docs: update changelog

---
+++ crates/lib/src/services/minetest_master_server/types.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Server {
+    pub address: String,
+    pub clients: u32,
+    pub clients_list: Vec<String>,
+    pub clients_max: u32,
+    pub creative: bool,
+    pub damage: bool,
+    pub description: String,
+    pub game_time: u32,
+    pub gameid: String,
+    pub lag: Option<f32>,
+    pub name: String,
+    pub password: bool,
+    pub port: u16,
+    pub proto_max: u16,
+    pub proto_min: u16,
+    pub pvp: bool,
+    pub uptime: u32,
+    pub url: Option<String>,
+    pub version: String,
+    pub ip: String,
+    pub update_time: u32,
+    pub start: u32,
+    pub clients_top: u32,
+    pub updates: u32,
+    pub total_clients: u32,
+    pub pop_v: f32,
+    pub geo_continent: Option<String>,
+    pub ping: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServersClients {
+    pub servers: u32,
+    pub clients: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Response {
+    pub total: ServersClients,
+    pub total_max: ServersClients,
+    pub list: Vec<Server>,
+}
--- crates/lib/src/services/mod.rs
+++ crates/lib/src/services/mod.rs
@@ -2,3 +2,7 @@
 
 /// Reference: [Master Server Query Protocol](https://developer.valvesoftware.com/wiki/Master_Server_Query_Protocol)
 pub mod valve_master_server;
+
+/// Reference: [Node-GameDig](https://github.com/gamedig/node-gamedig/blob/master/protocols/minetest.js)
+#[cfg(all(feature = "serde", feature = "tls"))]
+pub mod minetest_master_server;
