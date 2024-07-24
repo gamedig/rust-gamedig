93a2bd97e27c9a851ce44f1408e9ef4c4aeb689a
Cain <75994858+cainthebest@users.noreply.github.com>
2024-07-24 05:04:27 +0100

    chore(lib core): start adding docs to top lvl tcp

--- crates/lib/src/core/net/tcp/mod.rs
+++ crates/lib/src/core/net/tcp/mod.rs
@@ -1,3 +1,5 @@
+
+
 mod sealed;
 
 use sealed::client::Tcp;
@@ -13,6 +15,12 @@ pub(crate) struct TcpClient {
 
 #[maybe_async::maybe_async]
 impl TcpClient {
+    /// Creates a new TCP client instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `addr` - A reference to the `SocketAddr` of the server to connect to.
+    /// * `timeout` - An optional reference to the `Timeout` setting.
     #[allow(dead_code)]
     pub(crate) async fn new(addr: &SocketAddr, timeout: Option<&Timeout>) -> Result<Self> {
         Ok(Self {
@@ -20,11 +28,28 @@ impl TcpClient {
         })
     }
 
+    /// Reads data from the TCP stream.
+    ///
+    /// # Arguments
+    ///
+    /// * `size` - An optional size parameter indicating the number of bytes to
+    ///   read. If `None`, it will default to reading the maximum packet size.
+    ///
+    /// # Notes
+    ///
+    /// If the capacity of the sealed vec exceeds 120% of the size of the data
+    /// received, the vec may be reallocated as it will be shrunk to fit
+    /// the data size. This behavior is intended to optimize memory usage.
     #[allow(dead_code)]
     pub(crate) async fn read(&mut self, size: Option<u16>) -> Result<Vec<u8>> {
         self.client.inner.read(size).await
     }
 
+    /// Writes data to the TCP stream.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - A slice of bytes to be written to the TCP stream.
     #[allow(dead_code)]
     pub(crate) async fn write(&mut self, data: &[u8]) -> Result<()> {
         self.client.inner.write(data).await
