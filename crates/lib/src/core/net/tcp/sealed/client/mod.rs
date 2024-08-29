25090d3b06e808afa27d46f00d73a7fb953dc530
Cain <75994858+cainthebest@users.noreply.github.com>
2024-08-29 13:30:04 +0100

    fix(lib core): make tokio tcp more correct and simple

--- crates/lib/src/core/net/tcp/sealed/client/mod.rs
+++ crates/lib/src/core/net/tcp/sealed/client/mod.rs
@@ -2,8 +2,6 @@ use ::std::net::SocketAddr;
 
 use crate::{error::Result, settings::Timeout};
 
-use log::trace;
-
 #[cfg(feature = "client_std")]
 mod std;
 #[cfg(feature = "client_tokio")]
@@ -11,18 +9,15 @@ mod tokio;
 
 #[maybe_async::maybe_async]
 pub(crate) trait Tcp {
-    // MAX_TCP_PACKET_SIZE = ETHERNET_MTU - (IP_HEADER_SIZE + TCP_HEADER_SIZE)
-    const MAX_TCP_PACKET_SIZE: u16 = 1460;
-
-    // 100% = 128 or 1 << 7
-    const VEC_CAPACITY_BASE_UNIT: u8 = (1 << 7);
-    // 120% = 128 * 1.2 = 153.6 = 153
-    const VEC_CAPACITY_SHRINK_MARGIN: u8 = (Self::VEC_CAPACITY_BASE_UNIT as f32 * 1.2) as u8;
+    // The margin to shrink the buffer by
+    const BUF_SHRINK_MARGIN: u8 = 32;
+    // Default capacity for the buffer
+    const DEFAULT_BUF_CAPACITY: u16 = 1024;
 
     async fn new(addr: &SocketAddr, timeout: &Timeout) -> Result<Self>
     where Self: Sized;
 
-    async fn read(&mut self, size: Option<u16>) -> Result<Vec<u8>>;
+    async fn read(&mut self, size: Option<usize>) -> Result<Vec<u8>>;
     async fn write(&mut self, data: &[u8]) -> Result<()>;
 }
 
@@ -38,7 +33,10 @@ pub(crate) struct Inner {
 #[maybe_async::maybe_async]
 impl Inner {
     pub(crate) async fn new(addr: &SocketAddr, timeout: Option<&Timeout>) -> Result<Self> {
-        trace!("TCP::<Inner>::New: Creating new TCP client for {addr} with timeout: {timeout:?}");
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
+            "TCP::<Inner>::New: Creating new TCP client for {addr} with timeout: {timeout:?}"
+        );
 
         let timeout = timeout.unwrap_or(&Timeout::DEFAULT);
 
--- crates/lib/src/core/net/tcp/sealed/client/tokio.rs
+++ crates/lib/src/core/net/tcp/sealed/client/tokio.rs
@@ -1,4 +1,15 @@
-use std::{net::SocketAddr, sync::Arc, time::Duration};
+use std::{net::SocketAddr, time::Duration};
+
+use crate::{
+    error::{
+        diagnostic::{metadata::NetworkProtocol, FailureReason, Recommendation},
+        NetworkError,
+        Report,
+        Result,
+        ResultExt,
+    },
+    settings::Timeout,
+};
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -10,46 +21,66 @@ use tokio::{
     time::timeout as timer,
 };
 
-use crate::{
-    error::{NetworkError, Report, Result, _metadata::NetworkProtocol},
-    settings::Timeout,
-};
-
-use log::{debug, trace};
-
 #[derive(Debug)]
 pub(crate) struct TokioTcpClient {
     addr: SocketAddr,
     read_timeout: Duration,
     write_timeout: Duration,
-    read_stream: Arc<Mutex<OwnedReadHalf>>,
-    write_stream: Arc<Mutex<OwnedWriteHalf>>,
+    read_stream: Mutex<OwnedReadHalf>,
+    write_stream: Mutex<OwnedWriteHalf>,
 }
 
 #[maybe_async::async_impl]
 impl super::Tcp for TokioTcpClient {
     async fn new(addr: &SocketAddr, timeout: &Timeout) -> Result<Self> {
-        trace!("TCP::<Tokio>::New: Creating new TCP client for {addr} with timeout: {timeout:?}");
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
+            "TCP::<Tokio>::New: Creating new TCP client for {addr} with timeout: {timeout:?}"
+        );
 
         let (orh, owh) = match timer(timeout.connect, TcpStream::connect(addr)).await {
+            // Connection succeeded, split the stream into read and write halves.
             Ok(Ok(stream)) => stream.into_split(),
+
+            // Connection failed due to an IO error
             Ok(Err(e)) => {
-                return Err(Report::from(e).change_context(
-                    NetworkError::ConnectionError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: *addr,
-                    }
-                    .into(),
-                ));
+                let report = Report::from(e)
+                    .change_context(
+                        NetworkError::ConnectionError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: *addr,
+                        }
+                        .into(),
+                    )
+                    .attach_printable(FailureReason::new(
+                        "Failed to establish a TCP connection due to an underlying I/O error",
+                    ))
+                    .attach_printable(Recommendation::new(format!(
+                        "Verify the server address ({addr:?}) is reachable.",
+                    )));
+
+                return Err(report);
             }
+
+            // Connection failed due to a timeout
             Err(e) => {
-                return Err(Report::from(e).change_context(
-                    NetworkError::TimeoutElapsedError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: *addr,
-                    }
-                    .into(),
-                ));
+                return Err(Report::from(e)
+                    .change_context(
+                        NetworkError::TimeoutElapsedError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: *addr,
+                        }
+                        .into(),
+                    )
+                    .attach(FailureReason::new(
+                        "The connection attempt elapsed the timeout that was set.",
+                    ))
+                    .attach(Recommendation::new(
+                        "Check if the server is currently experiencing high traffic or \
+                         performance issues. Increase the timeout setting to accommodate delays, \
+                         especially during peak usage times or if the server is geographically \
+                         distant.",
+                    )));
             }
         };
 
@@ -57,84 +88,109 @@ impl super::Tcp for TokioTcpClient {
             addr: *addr,
             read_timeout: timeout.read,
             write_timeout: timeout.write,
-            read_stream: Arc::new(Mutex::new(orh)),
-            write_stream: Arc::new(Mutex::new(owh)),
+            read_stream: Mutex::new(orh),
+            write_stream: Mutex::new(owh),
         })
     }
 
-    async fn read(&mut self, size: Option<u16>) -> Result<Vec<u8>> {
-        trace!(
-            "TCP::<Std>::Read: Reading data from {} with size: {:?}",
-            self.addr,
-            size
+    async fn read(&mut self, size: Option<usize>) -> Result<Vec<u8>> {
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
+            "TCP::<Tokio>::Read: Reading data from {} with size: {size:?}",
+            self.addr
         );
 
-        let read_half = Arc::clone(&self.read_stream);
-        let mut orh = read_half.lock().await;
+        // Acquire a lock on the read stream
+        let mut orh_mg = self.read_stream.lock().await;
+        let orh = &mut *orh_mg;
 
-        let validated_size = match size {
-            Some(size) => size.min(Self::MAX_TCP_PACKET_SIZE) as usize,
-            None => Self::MAX_TCP_PACKET_SIZE as usize,
+        let valid_size = match size {
+            Some(size) => size,
+            None => Self::DEFAULT_BUF_CAPACITY as usize,
         };
 
-        let mut vec = Vec::with_capacity(validated_size);
+        let mut vec = Vec::with_capacity(valid_size);
 
         match timer(self.read_timeout, orh.read_to_end(&mut vec)).await {
+            // Data read successfully
             Ok(Ok(len)) => {
-                if validated_size < len {
-                    debug!(
-                        "TCP::<Std>::Read: More data than expected. Realloc was required. \
-                         Expected: {validated_size} bytes, Read: {len} bytes",
+                #[cfg(feature = "attribute_log")]
+                if valid_size < len {
+                    log::debug!(
+                        "TCP::<Tokio>::Read: Realloc was required, Requested: {valid_size}, \
+                         Received: {len} from {}",
+                        self.addr
                     );
                 }
 
-                let capacity = vec.capacity();
-                if capacity * (Self::VEC_CAPACITY_SHRINK_MARGIN as usize)
-                    > (len * Self::VEC_CAPACITY_BASE_UNIT as usize)
-                {
-                    debug!(
-                        "TCP::<Std>::Read: Shrink threshold exceeded. Shrinking vec to fit. \
-                         Capacity: {capacity}, Read: {len} bytes",
-                    );
-
+                // Shrink the vector to fit the data if there's excess capacity
+                if vec.capacity() > (len + Self::BUF_SHRINK_MARGIN as usize) {
                     vec.shrink_to_fit();
                 }
 
                 Ok(vec)
             }
+
+            // Error during the read operation
             Ok(Err(e)) => {
-                Err(Report::from(e).change_context(
+                return Err(Report::from(e).change_context(
                     NetworkError::ReadError {
                         _protocol: NetworkProtocol::Tcp,
                         addr: self.addr,
                     }
                     .into(),
                 ))
+                .attach(FailureReason::new(
+                    "An underlying IO error occurred during socket read operation.",
+                ));
             }
+
+            // Read operation timed out
             Err(e) => {
-                Err(Report::from(e).change_context(
-                    NetworkError::TimeoutElapsedError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: self.addr,
-                    }
-                    .into(),
-                ))
+                let report = Report::from(e)
+                    .change_context(
+                        NetworkError::TimeoutElapsedError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: self.addr,
+                        }
+                        .into(),
+                    )
+                    .attach(FailureReason::new(
+                        "The read operation had elapsed the timeout.",
+                    ));
+
+                // Needs to be chained as attaching moves the report
+                let report = if self.read_timeout < Timeout::DEFAULT_DURATION {
+                    report.attach(Recommendation::new(
+                        "Possibly increase the read timeout duration as the current duration set \
+                         is less than the default.",
+                    ))
+                } else {
+                    report
+                };
+
+                return Err(report);
             }
         }
     }
 
     async fn write(&mut self, data: &[u8]) -> Result<()> {
-        trace!(
-            "TCP::<Async>::Write: Writing data to {} with size: {}",
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
+            "TCP::<Tokio>::Write: Writing data to {} with size: {}",
             self.addr,
             data.len()
         );
 
-        let write_half = Arc::clone(&self.write_stream);
-        let mut owh = write_half.lock().await;
+        // Acquire a lock on the write stream
+        let mut owh_mg = self.write_stream.lock().await;
+        let owh = &mut *owh_mg;
 
         match timer(self.write_timeout, owh.write_all(data)).await {
+            // Data written successfully
             Ok(Ok(_)) => Ok(()),
+
+            // Error during the write operation
             Ok(Err(e)) => {
                 Err(Report::from(e).change_context(
                     NetworkError::WriteError {
@@ -143,15 +199,36 @@ impl super::Tcp for TokioTcpClient {
                     }
                     .into(),
                 ))
-            }
-            Err(e) => {
-                Err(Report::from(e).change_context(
-                    NetworkError::TimeoutElapsedError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: self.addr,
-                    }
-                    .into(),
+                .attach_printable(FailureReason::new(
+                    "An underlying IO error occurred during socket write operation.",
                 ))
+            }
+
+            // Write operation timed out
+            Err(e) => {
+                let report = Report::from(e)
+                    .change_context(
+                        NetworkError::TimeoutElapsedError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: self.addr,
+                        }
+                        .into(),
+                    )
+                    .attach(FailureReason::new(
+                        "The write operation had elapsed the timeout.",
+                    ));
+
+                // Needs to be chained as attaching moves the report
+                let report = if self.write_timeout < Timeout::DEFAULT_DURATION {
+                    report.attach(Recommendation::new(
+                        "Possibly increase the write timeout duration as the current duration set \
+                         is less than the default.",
+                    ))
+                } else {
+                    report
+                };
+
+                Err(report)
             }
         }
     }
