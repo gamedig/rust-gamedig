962a2c29882a8f6c6a6d130089ea2421907e77dc
Cain <75994858+cainthebest@users.noreply.github.com>
2024-08-30 21:57:01 +0100

    chore(lib core): add mods to prelude

--- crates/lib/src/core/prelude.rs
+++ crates/lib/src/core/prelude.rs
@@ -1,1 +1,8 @@
 
+#[allow(unused_imports)]
+#[cfg(feature = "_BUFFER")]
+pub(crate) use super::io::buffer::Buffer;
+
+#[allow(unused_imports)]
+#[cfg(feature = "_TCP")]
+pub(crate) use super::net::tcp::TcpClient;
