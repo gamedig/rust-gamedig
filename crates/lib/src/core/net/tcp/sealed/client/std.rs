d32d8e272151c0b58a14bdc29eebe84c6d495378
Cain <75994858+cainthebest@users.noreply.github.com>
2024-08-30 21:47:54 +0100

    refactor(lib tcp): use new errors with std internals

--- crates/lib/src/core/net/tcp/sealed/client/std.rs
+++ crates/lib/src/core/net/tcp/sealed/client/std.rs
@@ -4,11 +4,14 @@ use std::{
 };
 
 use crate::{
-    error::{NetworkError, Report, Result, _metadata::NetworkProtocol},
+    error::{
+        diagnostic::{metadata::NetworkProtocol, FailureReason, Recommendation},
+        NetworkError,
+        Report,
+        Result,
+    },
     settings::Timeout,
 };
-
-use log::{debug, trace};
 
 #[derive(Debug)]
 pub(crate) struct StdTcpClient {
@@ -19,33 +22,44 @@ pub(crate) struct StdTcpClient {
 #[maybe_async::sync_impl]
 impl super::Tcp for StdTcpClient {
     fn new(addr: &SocketAddr, timeout: &Timeout) -> Result<Self> {
-        trace!("TCP::<Std>::New: Creating new TCP client for {addr} with timeout: {timeout:?}");
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
+            "TCP::<Std>::New: Creating new TCP client for {addr} with timeout: {timeout:?}"
+        );
 
         match TcpStream::connect_timeout(addr, timeout.connect) {
             Ok(stream) => {
                 match stream.set_read_timeout(Some(timeout.read)) {
                     Ok(_) => {}
                     Err(e) => {
-                        return Err(Report::from(e).change_context(
-                            NetworkError::SetTimeoutError {
-                                _protocol: NetworkProtocol::Tcp,
-                                addr: *addr,
-                            }
-                            .into(),
-                        ));
+                        return Err(Report::from(e)
+                            .change_context(
+                                NetworkError::SetTimeoutError {
+                                    _protocol: NetworkProtocol::Tcp,
+                                    addr: *addr,
+                                }
+                                .into(),
+                            )
+                            .attach_printable(FailureReason::new(
+                                "Failed to set the read timeout for the TCP stream",
+                            )));
                     }
                 }
 
                 match stream.set_write_timeout(Some(timeout.write)) {
                     Ok(_) => {}
                     Err(e) => {
-                        return Err(Report::from(e).change_context(
-                            NetworkError::SetTimeoutError {
-                                _protocol: NetworkProtocol::Tcp,
-                                addr: *addr,
-                            }
-                            .into(),
-                        ));
+                        return Err(Report::from(e)
+                            .change_context(
+                                NetworkError::SetTimeoutError {
+                                    _protocol: NetworkProtocol::Tcp,
+                                    addr: *addr,
+                                }
+                                .into(),
+                            )
+                            .attach_printable(FailureReason::new(
+                                "Failed to set the write timeout for the TCP stream",
+                            )));
                     }
                 }
 
@@ -55,68 +69,74 @@ impl super::Tcp for StdTcpClient {
                 })
             }
             Err(e) => {
-                Err(Report::from(e).change_context(
-                    NetworkError::ConnectionError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: *addr,
-                    }
-                    .into(),
-                ))
+                Err(Report::from(e)
+                    .change_context(
+                        NetworkError::ConnectionError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: *addr,
+                        }
+                        .into(),
+                    )
+                    .attach_printable(FailureReason::new(
+                        "Failed to establish a TCP connection due to an underlying I/O error",
+                    ))
+                    .attach_printable(Recommendation::new(format!(
+                        "Verify the server address ({addr:?}) is reachable.",
+                    ))))
             }
         }
     }
 
-    fn read(&mut self, size: Option<u16>) -> Result<Vec<u8>> {
-        trace!(
+    fn read(&mut self, size: Option<usize>) -> Result<Vec<u8>> {
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
             "TCP::<Std>::Read: Reading data from {} with size: {:?}",
             self.addr,
             size
         );
 
-        let validated_size = match size {
-            Some(size) => size.min(Self::MAX_TCP_PACKET_SIZE) as usize,
-            None => Self::MAX_TCP_PACKET_SIZE as usize,
+        let valid_size = match size {
+            Some(size) => size,
+            None => Self::DEFAULT_BUF_CAPACITY as usize,
         };
 
-        let mut vec = Vec::with_capacity(validated_size);
+        let mut vec = Vec::with_capacity(valid_size);
 
         match self.stream.read_to_end(&mut vec) {
             Ok(len) => {
-                if validated_size < len {
-                    debug!(
+                #[cfg(feature = "attribute_log")]
+                if valid_size < len {
+                    log::debug!(
                         "TCP::<Std>::Read: More data than expected. Realloc was required. \
-                         Expected: {validated_size} bytes, Read: {len} bytes",
+                         Expected: {valid_size} bytes, Read: {len} bytes",
                     );
                 }
 
-                let capacity = vec.capacity();
-                if capacity * (Self::VEC_CAPACITY_SHRINK_MARGIN as usize)
-                    > (len * Self::VEC_CAPACITY_BASE_UNIT as usize)
-                {
-                    debug!(
-                        "TCP::<Std>::Read: Shrink threshold exceeded. Shrinking vec to fit. \
-                         Capacity: {capacity}, Read: {len} bytes",
-                    );
-
+                if vec.capacity() > (len + Self::BUF_SHRINK_MARGIN as usize) {
                     vec.shrink_to_fit();
                 }
 
                 Ok(vec)
             }
             Err(e) => {
-                Err(Report::from(e).change_context(
-                    NetworkError::ReadError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: self.addr,
-                    }
-                    .into(),
-                ))
+                Err(Report::from(e)
+                    .change_context(
+                        NetworkError::ReadError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: self.addr,
+                        }
+                        .into(),
+                    )
+                    .attach_printable(FailureReason::new(
+                        "An underlying IO error occurred during socket read operation.",
+                    )))
             }
         }
     }
 
     fn write(&mut self, data: &[u8]) -> Result<()> {
-        trace!(
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
             "TCP::<Std>::Write: Writing data to {} with size: {}",
             self.addr,
             data.len()
@@ -125,13 +145,17 @@ impl super::Tcp for StdTcpClient {
         match self.stream.write_all(data) {
             Ok(_) => Ok(()),
             Err(e) => {
-                Err(Report::from(e).change_context(
-                    NetworkError::WriteError {
-                        _protocol: NetworkProtocol::Tcp,
-                        addr: self.addr,
-                    }
-                    .into(),
-                ))
+                Err(Report::from(e)
+                    .change_context(
+                        NetworkError::WriteError {
+                            _protocol: NetworkProtocol::Tcp,
+                            addr: self.addr,
+                        }
+                        .into(),
+                    )
+                    .attach_printable(FailureReason::new(
+                        "An underlying IO error occurred during socket write operation.",
+                    )))
             }
         }
     }
