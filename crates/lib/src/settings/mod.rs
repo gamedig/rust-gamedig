44772c6637ba352c9f75cf05cfe0b900a167d550
Cain <75994858+cainthebest@users.noreply.github.com>
2024-08-30 22:30:45 +0100

    impl(lib): add settings struct

--- crates/lib/src/settings/mod.rs
+++ crates/lib/src/settings/mod.rs
@@ -1,3 +1,15 @@
 mod timeout;
 
 pub use timeout::Timeout;
+
+pub struct Settings {
+    pub timeout: Timeout,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            timeout: Timeout::default(),
+        }
+    }
+}
