3ef076ee8fb85bbf1b82b60efe95fdc087dc5f85
Cain <75994858+cainthebest@users.noreply.github.com>
2024-08-30 21:56:05 +0100

    fix(lib tcp): correct client logs

--- crates/lib/src/core/net/tcp/mod.rs
+++ crates/lib/src/core/net/tcp/mod.rs
@@ -5,8 +5,6 @@ use std::net::SocketAddr;
 
 use crate::{error::Result, settings::Timeout};
 
-use log::trace;
-
 #[allow(dead_code)]
 #[derive(Debug)]
 pub(crate) struct TcpClient {
@@ -23,7 +21,10 @@ impl TcpClient {
     /// * `timeout` - An optional reference to the `Timeout` setting.
     #[allow(dead_code)]
     pub(crate) async fn new(addr: &SocketAddr, timeout: Option<&Timeout>) -> Result<Self> {
-        trace!("TCP::<Client>::New: Creating new TCP client for {addr} with timeout: {timeout:?}");
+        #[cfg(feature = "attribute_log")]
+        log::trace!(
+            "TCP::<Client>::New: Creating new TCP client for {addr} with timeout: {timeout:?}"
+        );
 
         Ok(Self {
             client: sealed::client::Inner::new(addr, timeout).await?,
@@ -43,8 +44,9 @@ impl TcpClient {
     /// received, the vec may be reallocated as it will be shrunk to fit
     /// the data size. This behavior is intended to optimize memory usage.
     #[allow(dead_code)]
-    pub(crate) async fn read(&mut self, size: Option<u16>) -> Result<Vec<u8>> {
-        trace!("TCP::<Client>::Read: Reading data with size: {size:?}");
+    pub(crate) async fn read(&mut self, size: Option<usize>) -> Result<Vec<u8>> {
+        #[cfg(feature = "attribute_log")]
+        log::trace!("TCP::<Client>::Read: Reading data with size: {size:?}");
 
         self.client.inner.read(size).await
     }
@@ -56,7 +58,8 @@ impl TcpClient {
     /// * `data` - A slice of bytes to be written to the TCP stream.
     #[allow(dead_code)]
     pub(crate) async fn write(&mut self, data: &[u8]) -> Result<()> {
-        trace!("TCP::<Client>::Write: Writing data to the stream");
+        #[cfg(feature = "attribute_log")]
+        log::trace!("TCP::<Client>::Write: Writing data to the stream");
 
         self.client.inner.write(data).await
     }
