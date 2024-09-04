259f99649ffcd5a4742d2c3bb2cba3ff5527f204
Cain <75994858+cainthebest@users.noreply.github.com>
2024-09-04 22:30:10 +0100

    fix(lib http): set return type to json in al

--- crates/lib/src/core/net/http/sealed/client/mod.rs
+++ crates/lib/src/core/net/http/sealed/client/mod.rs
@@ -2,6 +2,8 @@ use super::super::{Body, Header, Query};
 
 use crate::error::Result;
 
+use serde_json::Value;
+
 #[allow(dead_code)]
 #[maybe_async::maybe_async]
 pub(crate) trait Http {
@@ -11,7 +13,7 @@ pub(crate) trait Http {
         url_query: Option<&Query>,
         header: Option<&Header>,
         body: Option<&Body>,
-    ) -> Result<String>;
+    ) -> Result<Value>;
 
     async fn post(
         &self,
@@ -19,7 +21,7 @@ pub(crate) trait Http {
         url_query: Option<&Query>,
         header: Option<&Header>,
         body: Option<&Body>,
-    ) -> Result<String>;
+    ) -> Result<Value>;
 
     async fn put(
         &self,
@@ -27,7 +29,7 @@ pub(crate) trait Http {
         url_query: Option<&Query>,
         header: Option<&Header>,
         body: Option<&Body>,
-    ) -> Result<String>;
+    ) -> Result<Value>;
 
     async fn delete(
         &self,
@@ -35,5 +37,5 @@ pub(crate) trait Http {
         url_query: Option<&Query>,
         header: Option<&Header>,
         body: Option<&Body>,
-    ) -> Result<String>;
+    ) -> Result<Value>;
 }
