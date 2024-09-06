786e9dad9487eafd1319b70a628b8d917b45821c
Cain <75994858+cainthebest@users.noreply.github.com>
2024-09-07 00:53:21 +0100

    fix(buf): use correct len fn on OOB check

--- crates/lib/src/buffer.rs
+++ crates/lib/src/buffer.rs
@@ -146,10 +146,10 @@ impl<'a, B: ByteOrder> Buffer<'a, B> {
     /// Returns a `BufferError` if there is an error decoding the string.
     pub fn read_string<D: StringDecoder>(&mut self, until: Option<D::Delimiter>) -> GDResult<String> {
         // Check if the cursor is out of bounds.
-        if self.cursor > self.remaining_length() {
+        if self.cursor > self.data_length() {
             return Err(PacketUnderflow.context(format!(
                 "Cursor position {} is out of bounds when reading string. Buffer length: {}",
-                self.cursor, self.data.len()
+                self.cursor, self.data_length()
             )));
         }
 
