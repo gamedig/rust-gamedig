e97314db0414dfc49495668c3e7ae808c08e6d3d
Cain <75994858+cainthebest@users.noreply.github.com>
2024-10-22 21:19:38 +0100

    fix(core buf): use pos as range context and add utils

--- crates/lib/src/core/io/buf/mod.rs
+++ crates/lib/src/core/io/buf/mod.rs
@@ -30,17 +30,46 @@ impl Buffer {
         }
     }
 
+    pub(crate) fn peek(&self, bytes: usize) -> Result<&[u8]> {
+        self.check_range(.. bytes)?;
+
+        Ok(&self.inner[self.pos .. self.pos + bytes])
+    }
+
+    pub(crate) fn move_pos(&mut self, pos: isize) -> Result<()> {
+        let new_pos = self.pos as isize + pos;
+
+        if new_pos < 0 || new_pos as usize > self.len {
+            return Err(Report::new(ErrorKind::from(IoError::BufferUnderflowError {
+                attempted: pos as usize,
+                available: self.len - self.pos,
+            }))
+            .attach_printable(FailureReason::new(
+                "Attempted to move the buffer position out of bounds.",
+            ))
+            .attach_printable(HexDump::new(
+                format!("Current buffer state (pos: {})", self.pos),
+                self.inner.clone(),
+            ))
+            .attach_printable(OpenGitHubIssue()));
+        }
+
+        self.pos = new_pos as usize;
+
+        Ok(())
+    }
+
     fn check_range<R>(&self, range: R) -> Result<()>
     where R: RangeBounds<usize> {
         let start = match range.start_bound() {
-            Bound::Included(&start) => start,
-            Bound::Excluded(&start) => start + 1,
+            Bound::Included(&start) => self.pos + start,
+            Bound::Excluded(_) => unreachable!(),
             Bound::Unbounded => self.pos,
         };
 
         let end = match range.end_bound() {
-            Bound::Included(&end) => end + 1,
-            Bound::Excluded(&end) => end,
+            Bound::Included(_) => unreachable!(),
+            Bound::Excluded(&end) => self.pos + end,
             Bound::Unbounded => self.len,
         };
 
