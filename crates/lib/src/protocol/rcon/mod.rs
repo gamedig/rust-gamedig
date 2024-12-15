63c2318ea5d2d8fb9b798db9bedeff2156ab08f2
Cain <75994858+cainthebest@users.noreply.github.com>
2024-12-15 16:54:51 +0000

    chore(rcon): remove unused

--- crates/lib/src/protocol/rcon/mod.rs
+++ crates/lib/src/protocol/rcon/mod.rs
@@ -1,7 +1,3 @@
-/// BattlEye RCON protocol.
-#[cfg(feature = "rcon_battleye_2")]
-pub mod battleye_2;
-
 /// Valve Source RCON protocol.
 #[cfg(feature = "rcon_valve_source")]
 pub mod valve_source;
