c3d02571a1f6d0452378eb712c386d5c444eb950
Dollan <dollan@duck.com>
2025-01-11 18:34:22 +0100

    Clearer error when parsing

--- crates/lib/examples/fivem.rs
+++ crates/lib/examples/fivem.rs
@@ -1,7 +1,7 @@
 use gamedig::games::fivem::query;
 
 fn main() {
-    let result = query(&"127.0.0.1".parse().unwrap(), None);
+    let result = query(&"127.0.0.1".parse().expect("Failed to parse IP"), None);
     match result {
         Ok(response) => {
             println!("Response: {:#?}", response);
