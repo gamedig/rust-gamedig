2f8a6d15dcc194ad01cb58d0cef84a896c08ac7c
Cain <75994858+cainthebest@users.noreply.github.com>
2025-02-07 15:06:33 +0000

    chore(rcon): remove for now as will come back to it

--- crates/lib/src/protocol/mod.rs
+++ crates/lib/src/protocol/mod.rs
@@ -6,11 +6,3 @@
 /// and other relevant data.
 pub mod query;
 
-/// RCON (Remote Console) protocol implementations.
-///
-/// This module provides the tools needed to interact with game servers via the 
-/// RCON protocol. RCON allows for remote command execution on game servers, 
-/// enabling server administrators to manage and control their servers 
-/// programmatically. This module includes functionalities for connecting 
-/// to servers, sending commands, and processing the responses.
-pub mod rcon;
--- crates/lib/src/protocol/rcon/mod.rs
+++
@@ -1,3 +0,0 @@
-/// Valve Source RCON protocol.
-#[cfg(feature = "rcon_valve_source")]
-pub mod valve_source;
