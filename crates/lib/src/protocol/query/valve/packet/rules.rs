526ae7133ea0f555bcab3022f29014f9debca4e2
Cain <75994858+cainthebest@users.noreply.github.com>
2025-03-10 19:39:22 +0000

    refactor(valve rules): simplify and just use hash map

--- crates/lib/src/protocol/query/valve/packet/rules.rs
+++ crates/lib/src/protocol/query/valve/packet/rules.rs
@@ -7,21 +7,5 @@
 /// - **Request Type:** `0x56`
 pub const RULES_REQUEST_PAYLOAD: [u8; 5] = [0xFF, 0xFF, 0xFF, 0xFF, 0x56];
 
-/// A single server rule represented as a key value pair.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Rule {
-    /// The name/key of the rule.
-    pub name: String,
-
-    /// The value associated with the rule.
-    pub value: String,
-}
-
-/// The complete response from a server rules query.
-///
-/// Contains a list of rules provided by the server.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Response {
-    /// A vector containing all the rules as key value pairs.
-    pub rules: Vec<Rule>,
-}
+/// Represents a list of server rules in key value format.
+pub type Rules = std::collections::HashMap<String, String>;
