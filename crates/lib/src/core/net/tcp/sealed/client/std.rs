658478b8d434b306833dc0ebeb2c75ef98b13f18
Cain <cainthebest@users.noreply.github.com>
2025-06-26 23:08:39 +0100

    refactor(std tcp): improve debug logs and errors

--- crates/lib/src/core/net/tcp/sealed/client/std.rs
+++ crates/lib/src/core/net/tcp/sealed/client/std.rs
@@ -3,7 +3,7 @@ use {
         NetworkError,
         Report,
         Result,
-        diagnostic::{FailureReason, Recommendation},
+        diagnostic::{CrateInfo, FailureReason, Recommendation, SystemInfo},
     },
     std::{
         io::{Read, Write},
@@ -23,95 +23,81 @@ pub(crate) struct StdTcpClient {
 
 #[maybe_async::sync_impl]
 impl super::AbstractTcp for StdTcpClient {
-    fn new(addr: &SocketAddr, timeout: Option<&Duration>) -> Result<Self> {
-        #[cfg(feature = "_DEV_LOG")]
-        log::trace!(
-            target: crate::log::EventTarget::GAMEDIG_DEV,
-            "TCP::<Std>::New: Creating new TCP client for {addr}"
+    fn new(addr: SocketAddr, timeout: Duration) -> Result<Self> {
+        dev_trace!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<NEW>: [addr: {addr:?}, timeout: \
+             {timeout:?}]",
         );
 
-        let valid_timeout = match timeout {
-            Some(timeout) => {
-                match timeout.is_zero() {
-                    true => Duration::from_secs(5),
-                    false => *timeout,
-                }
-            }
-
-            None => Duration::from_secs(5),
-        };
-
-        #[cfg(feature = "_DEV_LOG")]
-        log::debug!(
-            target: crate::log::EventTarget::GAMEDIG_DEV,
-            "TCP::<Std>::New: Attempting to connect to {addr:?} with a timeout of {valid_timeout:?}",
+        dev_debug!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<NEW>: Attempting to connect to \
+             {addr:?} with a timeout of {timeout:?}",
         );
 
-        match TcpStream::connect_timeout(addr, valid_timeout) {
+        match TcpStream::connect_timeout(&addr, timeout) {
             Ok(stream) => {
-                #[cfg(feature = "_DEV_LOG")]
-                log::debug!(
-                    target: crate::log::EventTarget::GAMEDIG_DEV,
-                    "TCP::<Std>::New: Successfully connected to {addr:?}",
+                dev_debug!(
+                    "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<NEW>: Successfully connected \
+                     to {addr:?}",
                 );
 
                 Ok(Self {
-                    peer_addr: *addr,
+                    peer_addr: addr,
                     stream,
                     read_timeout_set: false,
                     write_timeout_set: false,
                 })
             }
+
             Err(e) => {
                 return Err(Report::from(e)
-                    .change_context(NetworkError::TcpConnectionError { peer_addr: *addr }.into())
+                    .change_context(NetworkError::TcpConnectionError { peer_addr: addr }.into())
                     .attach_printable(FailureReason::new(
-                        "Failed to establish a TCP connection due to an underlying I/O error.",
+                        "Failed to establish a TCP connection due to an underlying OS I/O error.",
                     ))
                     .attach_printable(Recommendation::new(format!(
-                        "Verify the server address ({addr:?}) is reachable, ensure the server is \
+                        "Verify the server address ({addr}) is reachable, ensure the server is \
                          running, and that no firewall or network restrictions are blocking the \
-                         connection."
-                    ))));
+                         connection. Also consider increasing the timeout duration ({timeout:?}) \
+                         for distant or busy servers."
+                    )))
+                    .attach_printable(SystemInfo::new())
+                    .attach_printable(CrateInfo::new()));
             }
         }
     }
 
-    fn read_exact(&mut self, buf: &mut [u8], timeout: Option<&Duration>) -> Result<()> {
-        #[cfg(feature = "_DEV_LOG")]
-        log::trace!(
-            target: crate::log::EventTarget::GAMEDIG_DEV,
-            "TCP::<Std>::Read: Reading data from {}", &self.peer_addr);
+    fn read_exact(&mut self, buf: &mut [u8], timeout: Duration) -> Result<()> {
+        dev_trace!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: [buf: len({:?}), \
+             timeout: {timeout:?}]",
+            buf.len()
+        );
+
+        dev_debug!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: OS level socket read \
+             timeout status for {} : {}",
+            &self.peer_addr,
+            if self.read_timeout_set {
+                "already set"
+            } else {
+                "not set"
+            }
+        );
 
         // Set the read timeout if not already set
         if !self.read_timeout_set {
-            // Validate the timeout duration
-            let valid_timeout = match timeout {
-                Some(timeout) => {
-                    match timeout.is_zero() {
-                        true => Duration::from_secs(5),
-                        false => *timeout,
-                    }
-                }
-
-                None => Duration::from_secs(5),
-            };
-
-            #[cfg(feature = "_DEV_LOG")]
-            log::debug!(
-                target: crate::log::EventTarget::GAMEDIG_DEV,
-                "TCP::<Std>::Read: Setting read timeout for {} to {valid_timeout:?}",
+            dev_debug!(
+                "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: Attempting to set \
+                 read timeout for {} to {timeout:?}",
                 &self.peer_addr
-
-
             );
 
-            match self.stream.set_read_timeout(Some(valid_timeout)) {
+            match self.stream.set_read_timeout(Some(timeout)) {
                 Ok(_) => {
-                    #[cfg(feature = "_DEV_LOG")]
-                    log::debug!(
-                        target: crate::log::EventTarget::GAMEDIG_DEV,
-                        "TCP::<Std>::Read: Successfully set read timeout for {}",
+                    dev_debug!(
+                        "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: Successfully \
+                         set read timeout for {} to {timeout:?}",
                         &self.peer_addr
                     );
 
@@ -126,20 +112,33 @@ impl super::AbstractTcp for StdTcpClient {
                             }
                             .into(),
                         )
-                        .attach_printable(FailureReason::new(
-                            "Failed to set the read timeout for the TCP stream.",
-                        ))
-                        .attach_printable(Recommendation::new(
-                            "Ensure the timeout value is valid and that the stream is not in a \
-                             disconnected or invalid state.",
-                        )));
+                        .attach_printable(FailureReason::new(format!(
+                            "An underlying OS I/O error occurred while setting the read timeout \
+                             to {timeout:?} for the TCP stream."
+                        )))
+                        .attach_printable(SystemInfo::new())
+                        .attach_printable(CrateInfo::new()));
                 }
             }
         }
+
+        dev_debug!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: Attempting to read from \
+             {} with a timeout of {timeout:?}",
+            &self.peer_addr,
+        );
 
         match self.stream.read_exact(buf) {
             // Data read successfully
-            Ok(_) => Ok(()),
+            Ok(_) => {
+                dev_debug!(
+                    "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: Successfully \
+                     read data from {:?}",
+                    &self.peer_addr,
+                );
+
+                Ok(())
+            }
 
             // Error during the read operation
             Err(e) => {
@@ -156,46 +155,33 @@ impl super::AbstractTcp for StdTcpClient {
                     .attach_printable(Recommendation::new(
                         "Ensure the socket connection is stable and there are no issues with the \
                          network or server.",
-                    )))
+                    ))
+                    .attach_printable(SystemInfo::new())
+                    .attach_printable(CrateInfo::new()))
             }
         }
     }
 
-    fn read_to_end(&mut self, buf: &mut Vec<u8>, timeout: Option<&Duration>) -> Result<()> {
-        #[cfg(feature = "_DEV_LOG")]
-        log::trace!(
-            target: crate::log::EventTarget::GAMEDIG_DEV,
-            "TCP::<Std>::Read: Reading data from {}", &self.peer_addr);
+    fn read_to_end(&mut self, buf: &mut Vec<u8>, timeout: Duration) -> Result<()> {
+        dev_trace!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_TO_END>: [buf: cap({:?}), \
+             timeout: {timeout:?}]",
+            buf.capacity()
+        );
 
         // Set the read timeout if not already set
         if !self.read_timeout_set {
-            // Validate the timeout duration
-            let valid_timeout = match timeout {
-                Some(timeout) => {
-                    match timeout.is_zero() {
-                        true => Duration::from_secs(5),
-                        false => *timeout,
-                    }
-                }
-
-                None => Duration::from_secs(5),
-            };
-
-            #[cfg(feature = "_DEV_LOG")]
-            log::debug!(
-                target: crate::log::EventTarget::GAMEDIG_DEV,
-                "TCP::<Std>::Read: Setting read timeout for {} to {valid_timeout:?}",
+            dev_debug!(
+                "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_TO_END>: Attempting to set \
+                 read timeout for {} to {timeout:?}",
                 &self.peer_addr
-
-
             );
 
-            match self.stream.set_read_timeout(Some(valid_timeout)) {
+            match self.stream.set_read_timeout(Some(timeout)) {
                 Ok(_) => {
-                    #[cfg(feature = "_DEV_LOG")]
-                    log::debug!(
-                        target: crate::log::EventTarget::GAMEDIG_DEV,
-                        "TCP::<Std>::Read: Successfully set read timeout for {}",
+                    dev_debug!(
+                        "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_EXACT>: Successfully \
+                         set read timeout for {} to {timeout:?}",
                         &self.peer_addr
                     );
 
@@ -210,20 +196,34 @@ impl super::AbstractTcp for StdTcpClient {
                             }
                             .into(),
                         )
-                        .attach_printable(FailureReason::new(
-                            "Failed to set the read timeout for the TCP stream.",
-                        ))
-                        .attach_printable(Recommendation::new(
-                            "Ensure the timeout value is valid and that the stream is not in a \
-                             disconnected or invalid state.",
-                        )));
+                        .attach_printable(FailureReason::new(format!(
+                            "An underlying OS I/O error occurred while setting the read timeout \
+                             to {timeout:?} for the TCP stream."
+                        )))
+                        .attach_printable(SystemInfo::new())
+                        .attach_printable(CrateInfo::new()));
                 }
             }
         }
 
+        dev_debug!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_TO_END>: Attempting to read from \
+             {} with a timeout of {timeout:?}",
+            &self.peer_addr,
+        );
+
         match self.stream.read_to_end(buf) {
             // Data read successfully
-            Ok(_) => Ok(()),
+            Ok(_num_bytes) => {
+                dev_debug!(
+                    "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<READ_TO_END>: Successfully \
+                     read {} bytes from {}",
+                    _num_bytes,
+                    &self.peer_addr,
+                );
+
+                Ok(())
+            }
 
             // Error during the read operation
             Err(e) => {
@@ -240,41 +240,38 @@ impl super::AbstractTcp for StdTcpClient {
                     .attach_printable(Recommendation::new(
                         "Ensure the socket connection is stable and there are no issues with the \
                          network or server.",
-                    )))
+                    ))
+                    .attach_printable(SystemInfo::new())
+                    .attach_printable(CrateInfo::new()))
             }
         }
     }
 
-    fn write(&mut self, data: &[u8], timeout: Option<&Duration>) -> Result<()> {
-        #[cfg(feature = "_DEV_LOG")]
-        log::trace!(
-            target: crate::log::EventTarget::GAMEDIG_DEV,
-            "TCP::<Std>::Write: Writing data to {}", &self.peer_addr);
+    fn write(&mut self, data: &[u8], timeout: Duration) -> Result<()> {
+        dev_trace!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<WRITE>: [data: len({:?}), timeout: \
+             {timeout:?}]",
+            data.len()
+        );
 
         if !self.write_timeout_set {
-            // Validate the timeout duration
-            let valid_timeout = match timeout {
-                Some(timeout) => {
-                    match timeout.is_zero() {
-                        true => Duration::from_secs(5),
-                        false => *timeout,
-                    }
-                }
-
-                None => Duration::from_secs(5),
-            };
-
-            #[cfg(feature = "_DEV_LOG")]
-            log::debug!(
-                target: crate::log::EventTarget::GAMEDIG_DEV,
-                "TCP::<Std>::Write: Setting write timeout for {} to {valid_timeout:?}",
+            dev_debug!(
+                "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<WRITE>: Attempting to set write \
+                 timeout for {} to {timeout:?}",
                 &self.peer_addr
             );
 
-            match self.stream.set_write_timeout(Some(valid_timeout)) {
+            match self.stream.set_write_timeout(Some(timeout)) {
                 Ok(_) => {
+                    dev_debug!(
+                        "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<WRITE>: Successfully set \
+                         write timeout for {} to {timeout:?}",
+                        &self.peer_addr
+                    );
+
                     self.write_timeout_set = true;
                 }
+
                 Err(e) => {
                     return Err(Report::from(e)
                         .change_context(
@@ -283,19 +280,33 @@ impl super::AbstractTcp for StdTcpClient {
                             }
                             .into(),
                         )
-                        .attach_printable(FailureReason::new(
-                            "Failed to set the write timeout for the TCP stream.",
-                        ))
-                        .attach_printable(Recommendation::new(
-                            "Ensure the timeout value is valid and that the stream is not in a \
-                             disconnected or invalid state.",
-                        )));
+                        .attach_printable(FailureReason::new(format!(
+                            "An underlying OS I/O error occurred while setting the write timeout \
+                             to {timeout:?} for the TCP stream."
+                        )))
+                        .attach_printable(SystemInfo::new())
+                        .attach_printable(CrateInfo::new()));
                 }
             }
         }
 
+        dev_debug!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<WRITE>: Attempting to write to {} \
+             with a timeout of {timeout:?}",
+            &self.peer_addr,
+        );
+
         match self.stream.write_all(data) {
-            Ok(_) => Ok(()),
+            Ok(_) => {
+                dev_debug!(
+                    "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<WRITE>: Successfully wrote \
+                     data to {}",
+                    &self.peer_addr,
+                );
+
+                Ok(())
+            }
+
             Err(e) => {
                 Err(Report::from(e)
                     .change_context(
@@ -305,13 +316,24 @@ impl super::AbstractTcp for StdTcpClient {
                         .into(),
                     )
                     .attach_printable(FailureReason::new(
-                        "An underlying I/O error occurred during the socket write operation.",
+                        "An underlying RT or OS I/O error occurred during TCP write operation.",
                     ))
                     .attach_printable(Recommendation::new(
                         "Check if the server is accepting data correctly and ensure network \
                          stability.",
-                    )))
+                    ))
+                    .attach_printable(SystemInfo::new())
+                    .attach_printable(CrateInfo::new()))
             }
         }
+    }
+}
+
+impl Drop for StdTcpClient {
+    fn drop(&mut self) {
+        dev_debug!(
+            "GAMEDIG::CORE::NET::TCP::SEALED::CLIENT::STD::<DROP>: Dropping connection to {:?}",
+            &self.peer_addr,
+        );
     }
 }
