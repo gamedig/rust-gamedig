1234fbeb2919178d9039b097159c0d8ace60daee
Cain <cainthebest@users.noreply.github.com>
2025-08-22 15:11:53 +0100

    refactor(lib adapters): impl GenericPlayer and GenericServer data structures

--- crates/lib/src/adapters/player.rs
+++ crates/lib/src/adapters/player.rs
@@ -1,29 +1,37 @@
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 #[derive(Debug, Clone)]
-pub struct Player {
-    pub name: String,
-    pub score: Option<usize>,
-    pub deaths: Option<usize>,
-    pub ping: Option<Duration>,
+pub enum GenericPlayerDataValue {
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+    Usize(usize),
+
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    I128(i128),
+    Isize(isize),
+
+    F32(f32),
+    F64(f64),
+
+    Bool(bool),
+
+    String(String),
+
+    Duration(Duration),
 }
 
-pub trait IntoPlayer {
-    fn name(&self) -> String;
+#[derive(Debug, Clone)]
+pub struct GenericPlayer {
+    pub name: String,
+    pub data: Option<HashMap<String, GenericPlayerDataValue>>,
+}
 
-    fn score(&self) -> Option<usize> { None }
-
-    fn deaths(&self) -> Option<usize> { None }
-
-    fn ping(&self) -> Option<Duration> { None }
-
-    fn into_player(self) -> Player
-    where Self: Sized {
-        Player {
-            name: self.name(),
-            score: self.score(),
-            deaths: self.deaths(),
-            ping: self.ping(),
-        }
-    }
+pub trait IntoGenericPlayer: Sized {
+    fn into_generic_player(self) -> GenericPlayer;
 }
--- crates/lib/src/adapters/server.rs
+++ crates/lib/src/adapters/server.rs
@@ -1,1 +1,43 @@
+use {
+    super::player::GenericPlayer,
+    std::{collections::HashMap, net::SocketAddr, time::Duration},
+};
 
+#[derive(Debug, Clone)]
+pub enum GenericServerDataValue {
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+    Usize(usize),
+
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    I128(i128),
+    Isize(isize),
+
+    F32(f32),
+    F64(f64),
+
+    Bool(bool),
+
+    String(String),
+
+    Duration(Duration),
+
+    SocketAddr(SocketAddr),
+}
+
+#[derive(Debug, Clone)]
+pub struct GenericServer {
+    pub addr: SocketAddr,
+    pub data: Option<HashMap<String, GenericServerDataValue>>,
+    pub players: Option<Vec<GenericPlayer>>,
+}
+
+pub trait IntoGenericServer: Sized {
+    fn into_generic_server(self) -> GenericServer;
+}
