605be6cc6816db5ded944a5b0e305d3a74eea71d
Cain <cainthebest@users.noreply.github.com>
2025-11-25 07:54:44 +0000

    chore(games): update game and protocol features

--- crates/lib/src/game/mod.rs
+++ crates/lib/src/game/mod.rs
@@ -1,1 +1,260 @@
+#[cfg(feature = "game_abiotic_factor")]
+pub mod abiotic_factor;
 
+#[cfg(feature = "game_age_of_chivalry")]
+pub mod age_of_chivalry;
+
+#[cfg(feature = "game_alien_swarm")]
+pub mod alien_swarm;
+
+#[cfg(feature = "game_alien_swarm_reactive_drop")]
+pub mod alien_swarm_reactive_drop;
+
+#[cfg(feature = "game_aliens_vs_predator_2010")]
+pub mod aliens_vs_predator_2010;
+
+#[cfg(feature = "game_americas_army_proving_grounds")]
+pub mod americas_army_proving_grounds;
+
+#[cfg(feature = "game_ark_survival_ascended")]
+pub mod ark_survival_ascended;
+
+#[cfg(feature = "game_ark_survival_evolved")]
+pub mod ark_survival_evolved;
+
+#[cfg(feature = "game_arma_2_operation_arrowhead")]
+pub mod arma_2_operation_arrowhead;
+
+#[cfg(feature = "game_arma_3")]
+pub mod arma_3;
+
+#[cfg(feature = "game_arma_reforger")]
+pub mod arma_reforger;
+
+#[cfg(feature = "game_atlas")]
+pub mod atlas;
+
+#[cfg(feature = "game_avorion")]
+pub mod avorion;
+
+#[cfg(feature = "game_ballistic_overkill")]
+pub mod ballistic_overkill;
+
+#[cfg(feature = "game_barotrauma")]
+pub mod barotrauma;
+
+#[cfg(feature = "game_base_defense")]
+pub mod base_defense;
+
+#[cfg(feature = "game_battalion_1944")]
+pub mod battalion_1944;
+
+#[cfg(feature = "game_battlefield_1942")]
+pub mod battlefield_1942;
+
+#[cfg(feature = "game_black_mesa")]
+pub mod black_mesa;
+
+#[cfg(feature = "game_brainbread_2")]
+pub mod brainbread_2;
+
+#[cfg(feature = "game_call_of_duty_black_ops_3")]
+pub mod call_of_duty_black_ops_3;
+
+#[cfg(feature = "game_codename_cure")]
+pub mod codename_cure;
+
+#[cfg(feature = "game_colony_survival")]
+pub mod colony_survival;
+
+#[cfg(feature = "game_conan_exiles")]
+pub mod conan_exiles;
+
+#[cfg(feature = "game_counter_strike")]
+pub mod counter_strike;
+
+#[cfg(feature = "game_counter_strike_2")]
+pub mod counter_strike_2;
+
+#[cfg(feature = "game_counter_strike_condition_zero")]
+pub mod counter_strike_condition_zero;
+
+#[cfg(feature = "game_counter_strike_global_offensive")]
+pub mod counter_strike_global_offensive;
+
+#[cfg(feature = "game_counter_strike_source")]
+pub mod counter_strike_source;
+
+#[cfg(feature = "game_creativerse")]
+pub mod creativerse;
+
+#[cfg(feature = "game_crysis_wars")]
+pub mod crysis_wars;
+
+#[cfg(feature = "game_day_of_defeat")]
+pub mod day_of_defeat;
+
+#[cfg(feature = "game_day_of_defeat_source")]
+pub mod day_of_defeat_source;
+
+#[cfg(feature = "game_day_of_infamy")]
+pub mod day_of_infamy;
+
+#[cfg(feature = "game_dont_starve_together")]
+pub mod dont_starve_together;
+
+#[cfg(feature = "game_double_action_boogaloo")]
+pub mod double_action_boogaloo;
+
+#[cfg(feature = "game_eco")]
+pub mod eco;
+
+#[cfg(feature = "game_enshrouded")]
+pub mod enshrouded;
+
+#[cfg(feature = "game_garrys_mod")]
+pub mod garrys_mod;
+
+#[cfg(feature = "game_half_life_2_deathmatch")]
+pub mod half_life_2_deathmatch;
+
+#[cfg(feature = "game_half_life_deathmatch_source")]
+pub mod half_life_deathmatch_source;
+
+#[cfg(feature = "game_halo_combat_evolved")]
+pub mod halo_combat_evolved;
+
+#[cfg(feature = "game_hell_let_loose")]
+pub mod hell_let_loose;
+
+#[cfg(feature = "game_insurgency")]
+pub mod insurgency;
+
+#[cfg(feature = "game_insurgency_modern_infantry_combat")]
+pub mod insurgency_modern_infantry_combat;
+
+#[cfg(feature = "game_insurgency_sandstorm")]
+pub mod insurgency_sandstorm;
+
+#[cfg(feature = "game_left_4_dead")]
+pub mod left_4_dead;
+
+#[cfg(feature = "game_left_4_dead_2")]
+pub mod left_4_dead_2;
+
+#[cfg(feature = "game_minecraft_bedrock")]
+pub mod minecraft_bedrock;
+
+#[cfg(feature = "game_minecraft_java")]
+pub mod minecraft_java;
+
+#[cfg(feature = "game_minecraft_java_v_1_4")]
+pub mod minecraft_java_v_1_4;
+
+#[cfg(feature = "game_minecraft_java_v_1_6")]
+pub mod minecraft_java_v_1_6;
+
+#[cfg(feature = "game_minecraft_java_v_1_8_beta")]
+pub mod minecraft_java_v_1_8_beta;
+
+#[cfg(feature = "game_mordhau")]
+pub mod mordhau;
+
+#[cfg(feature = "game_myth_of_empires")]
+pub mod myth_of_empires;
+
+#[cfg(feature = "game_nova_life_amboise")]
+pub mod nova_life_amboise;
+
+#[cfg(feature = "game_onset")]
+pub mod onset;
+
+#[cfg(feature = "game_operation_harsh_doorstop")]
+pub mod operation_harsh_doorstop;
+
+#[cfg(feature = "game_pirates_vikings_and_knights_ii")]
+pub mod pirates_vikings_and_knights_ii;
+
+#[cfg(feature = "game_pixark")]
+pub mod pixark;
+
+#[cfg(feature = "game_post_scriptum")]
+pub mod post_scriptum;
+
+#[cfg(feature = "game_project_zomboid")]
+pub mod project_zomboid;
+
+#[cfg(feature = "game_quake_1")]
+pub mod quake_1;
+
+#[cfg(feature = "game_quake_2")]
+pub mod quake_2;
+
+#[cfg(feature = "game_quake_3_arena")]
+pub mod quake_3_arena;
+
+#[cfg(feature = "game_rising_world")]
+pub mod rising_world;
+
+#[cfg(feature = "game_risk_of_rain_2")]
+pub mod risk_of_rain_2;
+
+#[cfg(feature = "game_rust")]
+pub mod rust;
+
+#[cfg(feature = "game_savage_2")]
+pub mod savage_2;
+
+#[cfg(feature = "game_serious_sam")]
+pub mod serious_sam;
+
+#[cfg(feature = "game_seven_days_to_die")]
+pub mod seven_days_to_die;
+
+#[cfg(feature = "game_soldier_of_fortune_2")]
+pub mod soldier_of_fortune_2;
+
+#[cfg(feature = "game_soulmask")]
+pub mod soulmask;
+
+#[cfg(feature = "game_squad")]
+pub mod squad;
+
+#[cfg(feature = "game_starbound")]
+pub mod starbound;
+
+#[cfg(feature = "game_sven_co_op")]
+pub mod sven_co_op;
+
+#[cfg(feature = "game_team_fortress_2")]
+pub mod team_fortress_2;
+
+#[cfg(feature = "game_team_fortress_classic")]
+pub mod team_fortress_classic;
+
+#[cfg(feature = "game_the_forest")]
+pub mod the_forest;
+
+#[cfg(feature = "game_the_front")]
+pub mod the_front;
+
+#[cfg(feature = "game_the_ship")]
+pub mod the_ship;
+
+#[cfg(feature = "game_unreal_tournament")]
+pub mod unreal_tournament;
+
+#[cfg(feature = "game_unturned")]
+pub mod unturned;
+
+#[cfg(feature = "game_v_rising")]
+pub mod v_rising;
+
+#[cfg(feature = "game_valheim")]
+pub mod valheim;
+
+#[cfg(feature = "game_warsow")]
+pub mod warsow;
+
+#[cfg(feature = "game_zombie_panic_source")]
+pub mod zombie_panic_source;
