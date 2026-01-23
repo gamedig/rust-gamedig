875efa1bdad6f53fa66ddcff4e326b37d62d0add
Cain <cainthebest@users.noreply.github.com>
2026-01-23 02:18:43 +0000

    refactor(core): reorganize module imports for better clarity and structure

--- crates/lib/src/core/mod.rs
+++ crates/lib/src/core/mod.rs
@@ -1,6 +1,22 @@
 pub(crate) mod error;
-pub(crate) mod http;
-pub(crate) mod tcp;
-pub(crate) mod udp;
 
+#[cfg(feature = "_HTTP")]
+pub(crate) mod http;
+
+#[allow(unused)]
+#[cfg(feature = "_HTTP")]
+pub(crate) use http::{Form, Headers, HttpClient, Payload, Query};
+
+#[cfg(feature = "_TCP")]
+pub(crate) mod tcp;
+#[allow(unused)]
+#[cfg(feature = "_TCP")]
+pub(crate) use tcp::TcpClient;
+
+#[cfg(feature = "_UDP")]
+pub(crate) mod udp;
+#[cfg(feature = "_UDP")]
+pub(crate) use udp::UdpClient;
+
+#[cfg(feature = "_BUFFER")]
 pub(crate) mod buffer;
