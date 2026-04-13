3eb5e02dc8df9639e69beb59dac0c3aad83368f2
Cain <cainthebest@users.noreply.github.com>
2026-04-13 22:44:32 +0100

    refactor(udp): update feature flags for module imports

--- crates/lib/src/core/udp/client/mod.rs
+++ crates/lib/src/core/udp/client/mod.rs
@@ -4,15 +4,15 @@ use {
     ::std::{net::SocketAddr, time::Duration},
 };
 
-#[cfg(feature = "socket_std")]
+#[cfg(feature = "rt_std")]
 mod std;
-#[cfg(feature = "socket_tokio")]
+#[cfg(feature = "rt_tokio")]
 mod tokio;
 
-#[cfg(feature = "socket_std")]
+#[cfg(feature = "rt_std")]
 pub(crate) type InnerUdpClient = std::StdUdpClient;
 
-#[cfg(feature = "socket_tokio")]
+#[cfg(feature = "rt_tokio")]
 pub(crate) type InnerUdpClient = tokio::TokioUdpClient;
 
 #[maybe_async::maybe_async]
