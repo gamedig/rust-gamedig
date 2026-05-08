e8eec27fb7551f8ee91e7e1f29762a28113a888b
Cain <cainthebest@users.noreply.github.com>
2026-05-08 21:07:04 +0100

    refactor(lib build script): clean up use

--- crates/lib/build.rs
+++ crates/lib/build.rs
@@ -1,7 +1,7 @@
 #[path = "build/mod.rs"]
 mod build;
 
-use {build::Task, build::export_build_target::BuildTarget, build::generate_feature_list::Features};
+use build::{Task, export_build_target::BuildTarget, generate_feature_list::Features};
 
 pub(crate) const TASKS: &[fn()] = &[BuildTarget::run, Features::run];
 
