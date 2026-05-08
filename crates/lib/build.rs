3662b2091bb8978b161f90037d9f5b180a81479e
Cain <cainthebest@users.noreply.github.com>
2026-05-08 21:08:15 +0100

    refactor(lib build script): change TASKS to priv

--- crates/lib/build.rs
+++ crates/lib/build.rs
@@ -3,7 +3,7 @@ mod build;
 
 use build::{Task, export_build_target::BuildTarget, generate_feature_list::Features};
 
-pub(crate) const TASKS: &[fn()] = &[BuildTarget::run, Features::run];
+const TASKS: &[fn()] = &[BuildTarget::run, Features::run];
 
 fn main() {
     for task in TASKS {
